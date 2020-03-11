fc65b53690ec9925fe1b75e61046a7af44003b13
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2020-03-11 09:06:10 +0000

    Update to latest software

---
+++ app/src/main/java/com/mvc/cryptovault/app/AppBootstrap.java
@@ -0,0 +1,6 @@
+package com.mvc.cryptovault.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.ci
