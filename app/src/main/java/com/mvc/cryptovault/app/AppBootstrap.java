1356a127a78431809074137f2aee1c08d80cf16a
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2020-03-11 14:34:57 +0000

    fix cli

--- app/src/main/java/com/mvc/cryptovault/app/AppBootstrap.java
+++ app/src/main/java/com/mvc/cryptovault/app/AppBootstrap.java
@@ -13,4 +13,25 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-/*
+/**
+ * @author qiyichen
+ * @create 2018/11/5 16:10
+ */
+//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableSwagger2
+@EnableAsync
+@Configuration
+@ComponentScan({"com.mvc.cryptovault.common.swaggermock", "com.mvc.cryptovault"})
+@EnableCircuitBreaker
+@EnableHystrix
+@EnableEurekaClient
+@EnableFeignClients
+@EnableDiscoveryClient
+public class AppBootstrap {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppBootstrap.class, args);
+    }
+
+}
