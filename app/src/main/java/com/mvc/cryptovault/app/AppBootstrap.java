0d960856e0a5b3c28723bdb32fac0c6694a0b684
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2020-03-11 13:32:13 +0000

    restaurando post

--- app/src/main/java/com/mvc/cryptovault/app/AppBootstrap.java
+++ app/src/main/java/com/mvc/cryptovault/app/AppBootstrap.java
@@ -3,4 +3,14 @@ package com.mvc.cryptovault.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.client.ci
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/*
