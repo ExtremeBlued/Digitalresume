0c95d06e276f57529f776dd93eb1eaf7d1d3460f
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2020-03-12 11:41:21 +0000

    Fixed compilation warning

---
+++ app/src/main/java/com/mvc/cryptovault/app/config/FeignAutoConfig.java
@@ -0,0 +1,7 @@
+package com.mvc.cryptovault.app.config;
+
+import feign.Feign;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
