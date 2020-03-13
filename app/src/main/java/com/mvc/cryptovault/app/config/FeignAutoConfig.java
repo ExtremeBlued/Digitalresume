58a8cf8b19e6fab198ecdc97174c94a7d23098e2
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2020-03-13 09:18:40 +0000

    update: README

--- app/src/main/java/com/mvc/cryptovault/app/config/FeignAutoConfig.java
+++ app/src/main/java/com/mvc/cryptovault/app/config/FeignAutoConfig.java
@@ -5,3 +5,12 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@ConditionalOnClass(Feign.class)
+public class FeignAutoConfig {
+
+    @Bean
+    public CharlesRequestInterceptor charlesRequestInterceptor(){
+        return new CharlesRequestInterceptor();
+    }
+}
