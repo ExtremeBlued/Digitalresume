0ae718462e297fccc2d3ccf3ac27ce76397aecdc
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2020-03-22 21:32:36 +0000

    ipv6: fix exthdrs offload registration in out_rt path

--- app/src/main/java/com/mvc/cryptovault/app/config/GlobalExceptionHandler.java
+++ app/src/main/java/com/mvc/cryptovault/app/config/GlobalExceptionHandler.java
@@ -7,4 +7,24 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStat
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.security.auth.login.LoginException;
+
+/**
+ * @author qyc
+ */
+@ControllerAdvice
+@ResponseBody
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(LoginException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public Result loginExceptionException() {
+        return new Result(HttpStatus.UNAUTHORIZED.value(), "token error", null);
+    }
+
+    @ExceptionHandler(TokenErrorException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public Result tokenErrorExceptionException() {
+        return new Result(HttpStatus.FORBIDDEN.value(), "token error", null
