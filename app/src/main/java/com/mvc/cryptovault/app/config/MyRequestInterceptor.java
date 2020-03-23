fed14c7055420bb20b81f944cbf6a3f6eaba8203
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2020-03-23 09:12:31 +0000

    Address review comments

--- app/src/main/java/com/mvc/cryptovault/app/config/MyRequestInterceptor.java
+++ app/src/main/java/com/mvc/cryptovault/app/config/MyRequestInterceptor.java
@@ -9,4 +9,13 @@ import org.springframework.context.i18n.LocaleContextHolder;
  * bean confing
  *
  * @author qiyichen
- * @creat
+ * @create 2018/3/12 19:38
+ */
+@Configuration
+public class MyRequestInterceptor implements RequestInterceptor {
+
+    @Override
+    public void apply(RequestTemplate requestTemplate) {
+        requestTemplate.header("Accept-Language", LocaleContextHolder.getLocale().toString());
+    }
+}
---
+++ app/src/main/java/com/mvc/cryptovault/app/config/RedisConfig.java
@@ -0,0 +1,8 @@
+package com.mvc.cryptovault.app.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.r
