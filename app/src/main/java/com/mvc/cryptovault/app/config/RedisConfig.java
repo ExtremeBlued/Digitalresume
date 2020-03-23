1666ddf6f8d3c34d4615a09bbb83e05e95b641d4
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2020-03-23 21:06:02 +0000

    sql

--- app/src/main/java/com/mvc/cryptovault/app/config/RedisConfig.java
+++ app/src/main/java/com/mvc/cryptovault/app/config/RedisConfig.java
@@ -5,4 +5,19 @@ import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.r
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+
+/**
+ * <p>redis缓存配置</p>
+ * Created by zhezhiyong@163.com on 2017/9/21.
+ */
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
+        StringRedisTemplate template = new StringRedisTemplate(factory);
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new 
