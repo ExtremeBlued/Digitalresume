cde339308ffd243b089174b0ae18e08cd01df5d8
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2020-03-28 09:27:50 +0000

    Update pytest-cov from 2.6.1 to 2.7.1

--- app/src/main/java/com/mvc/cryptovault/app/controller/MessageController.java
+++ app/src/main/java/com/mvc/cryptovault/app/controller/MessageController.java
@@ -13,4 +13,20 @@ import java.math.BigInteger;
 import java.util.List;
 
 /**
- * 消息相
+ * 消息相关
+ *
+ * @author qiyichen
+ * @create 2018/11/8 14:23
+ */
+@RestController
+@Api(tags = "消息相关")
+@RequestMapping("message")
+public class MessageController extends BaseController {
+
+    @ApiOperation("传入时间戳获取通知信息,本地需要保存已读状态,服务器已读状态变更优先度很低")
+    @GetMapping
+    @SwaggerMock("${message.list}")
+    public Result<List<MessageVO>> getlist(@ModelAttribute TimeSearchDTO timeSearchDTO, @ModelAttribute PageDTO pageDTO) {
+        BigInteger userId = getUserId();
+        List<MessageVO> list = messageService.getlist(userId, timeSearchDTO.getTimestamp(), timeSearchDTO.getType(), pageDTO.getPageSize());
+        return new Result<>(list);
