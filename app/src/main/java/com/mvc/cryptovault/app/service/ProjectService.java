4c6133e5333d2067ceaa0a72fe7ba048713a6339
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2020-04-24 13:31:52 +0000

    fix undefined

--- app/src/main/java/com/mvc/cryptovault/app/service/ProjectService.java
+++ app/src/main/java/com/mvc/cryptovault/app/service/ProjectService.java
@@ -3,4 +3,19 @@ package com.mvc.cryptovault.app.service;
 import com.github.pagehelper.PageInfo;
 import com.mvc.cryptovault.app.feign.ConsoleRemoteService;
 import com.mvc.cryptovault.common.bean.AppProject;
-import com.mvc.cryptovault.com
+import com.mvc.cryptovault.common.bean.dto.ProjectBuyDTO;
+import com.mvc.cryptovault.common.bean.dto.ProjectDTO;
+import com.mvc.cryptovault.common.bean.dto.ReservationDTO;
+import com.mvc.cryptovault.common.bean.vo.ProjectBuyVO;
+import com.mvc.cryptovault.common.bean.vo.ProjectSimpleVO;
+import com.mvc.cryptovault.common.bean.vo.PurchaseVO;
+import com.mvc.cryptovault.common.bean.vo.Result;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
+@Ser
