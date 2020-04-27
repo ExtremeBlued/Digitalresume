a13078d9fc6a4a366c9d2107aecd11201aa771f9
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2020-04-27 19:30:25 +0000

    remove log line

--- app/src/main/java/com/mvc/cryptovault/app/service/TransactionService.java
+++ app/src/main/java/com/mvc/cryptovault/app/service/TransactionService.java
@@ -1,3 +1,20 @@
 package com.mvc.cryptovault.app.service;
 
-import com.mvc.cryptovault.app.feign.ConsoleRemote
+import com.mvc.cryptovault.app.feign.ConsoleRemoteService;
+import com.mvc.cryptovault.common.bean.dto.MyTransactionDTO;
+import com.mvc.cryptovault.common.bean.dto.OrderDTO;
+import com.mvc.cryptovault.common.bean.dto.PairDTO;
+import com.mvc.cryptovault.common.bean.dto.TransactionBuyDTO;
+import com.mvc.cryptovault.common.bean.vo.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigInteger;
+import java.util.List;
+
+@Service
+public class TransactionService {
+
+    @Autowired
+    ConsoleRemoteService transactionRemoteService;
+
