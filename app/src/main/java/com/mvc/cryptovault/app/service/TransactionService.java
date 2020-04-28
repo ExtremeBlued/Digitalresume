b14fc4859b5f495418c62d246e798e51f2033ab6
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2020-04-28 07:55:35 +0000

    Drivers: hv: vmbus: Fix a bug in the error path in vmbus_open()

--- app/src/main/java/com/mvc/cryptovault/app/service/TransactionService.java
+++ app/src/main/java/com/mvc/cryptovault/app/service/TransactionService.java
@@ -18,3 +18,19 @@ public class TransactionService {
     @Autowired
     ConsoleRemoteService transactionRemoteService;
 
+    public List<PairVO> getPair(BigInteger userId, PairDTO pairDTO) {
+        Result<List<PairVO>> result = transactionRemoteService.getPair(pairDTO);
+        return result.getData();
+    }
+
+    public List<OrderVO> getTransactions(BigInteger userId, OrderDTO dto) {
+        Result<List<OrderVO>> result = transactionRemoteService.getTransactions(dto);
+        return result.getData();
+    }
+
+    public KLineVO getKLine(BigInteger userId, BigInteger pairId) {
+        Result<KLineVO> result = transactionRemoteService.getTransactions(pairId);
+        return result.getData();
+    }
+
+    public Li
