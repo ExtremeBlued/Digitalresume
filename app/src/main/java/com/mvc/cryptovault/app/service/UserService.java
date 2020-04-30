b1b23141199053ec47403c26d7dcfbed9c48f8cd
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2020-04-30 08:20:43 +0000

    Added TODO comments

--- app/src/main/java/com/mvc/cryptovault/app/service/UserService.java
+++ app/src/main/java/com/mvc/cryptovault/app/service/UserService.java
@@ -6,4 +6,22 @@ import com.mvc.cryptovault.common.bean.dto.UserDTO;
 import com.mvc.cryptovault.common.bean.vo.Result;
 import com.mvc.cryptovault.common.bean.vo.TokenVO;
 import com.mvc.cryptovault.common.bean.vo.UserSimpleVO;
-import com.mvc.cryptovault.common.
+import com.mvc.cryptovault.common.util.BaseContextHandler;
+import com.mvc.cryptovault.common.util.JwtHelper;
+import com.mvc.cryptovault.common.util.MessageConstants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+import javax.security.auth.login.LoginException;
+import java.math.BigInteger;
+
+@Service
+public class UserService {
+
+    @Autowired
+    ConsoleRemoteService userRemoteService;
+    @Autowired
+    StringRedisTemplate redisTemplate;
+
