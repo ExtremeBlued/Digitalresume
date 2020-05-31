6c75ff77eeb67a49a4c3d187add4358a128429f7
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2020-05-31 22:44:36 +0000

    1.2

--- app/src/test/java/com/mvc/cryptovault/app/controller/TokenControllerTest.java
+++ app/src/test/java/com/mvc/cryptovault/app/controller/TokenControllerTest.java
@@ -4,4 +4,20 @@ import com.alibaba.fastjson.TypeReference;
 import com.mvc.cryptovault.app.base.BaseTest;
 import com.mvc.cryptovault.common.bean.vo.TokenDetailVO;
 import org.junit.Test;
-import org.springframework.
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+public class TokenControllerTest extends BaseTest {
+
+    @Test
+    public void getTokens() throws Exception {
+        //查询所有
+        MvcRes
