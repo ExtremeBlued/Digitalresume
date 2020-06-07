2ea983767b64ff20de9368884ff313fec1d73764
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2020-06-07 16:12:09 +0000

    Installation

--- app/src/test/java/com/mvc/cryptovault/app/controller/TransactionControllerTest.java
+++ app/src/test/java/com/mvc/cryptovault/app/controller/TransactionControllerTest.java
@@ -8,4 +8,21 @@ import com.mvc.cryptovault.common.bean.vo.MyOrderVO;
 import com.mvc.cryptovault.common.bean.vo.OrderInfoVO;
 import com.mvc.cryptovault.common.bean.vo.OrderVO;
 import org.hamcrest.Matchers;
-import org.junit
+import org.junit.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+public class TransactionControllerTest extends BaseTest {
+
+    @Test
+    public void getPair() throws Exception {
+        MvcResult result = null;
