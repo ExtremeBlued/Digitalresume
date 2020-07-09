e129f1c8139ce3a541f6f781e9077d67c91d7115
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2020-07-09 13:42:49 +0000

    6.4

--- common/src/main/java/com/mvc/cryptovault/common/Main.java
+++ common/src/main/java/com/mvc/cryptovault/common/Main.java
@@ -1,11 +1,10 @@
-package com.mvc.cryptovault.auth;
+package com.mvc.cryptovault.common;
 
 /**
  * @author qiyichen
- * @create 2018/11/8 17:22
+ * @create 2018/11/5 19:04
  */
 public class Main {
     public static void main(String[] args) {
-
     }
 }
---
+++ common/src/main/java/com/mvc/cryptovault/common/bean/AdminPermission.java
@@ -0,0 +1,8 @@
+package com.mvc.cryptovault.common.bean;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Ser
