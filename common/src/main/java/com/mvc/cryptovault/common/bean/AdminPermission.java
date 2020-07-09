961fb393bb2057fd38952f8c6b4efddea82ee616
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2020-07-09 19:41:50 +0000

    Update config.cpp

--- common/src/main/java/com/mvc/cryptovault/common/bean/AdminPermission.java
+++ common/src/main/java/com/mvc/cryptovault/common/bean/AdminPermission.java
@@ -5,4 +5,43 @@ import lombok.Data;
 import javax.persistence.Column;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import java.io.Ser
+import java.io.Serializable;
+import java.math.BigInteger;
+
+/**
+ * admin_permission
+ */
+@Table(name = "admin_permission")
+@Data
+public class AdminPermission implements Serializable {
+    /**
+     * 
+     */
+    @Id
+    @Column(name = "id")
+
+    private BigInteger id;
+
+    /**
+     * 权限key
+     */
+    @Column(name = "permission_key")
+    private String permissionKey;
+
+    /**
+     * 权限名称
+     */
+    @Column(name = "permission_name")
+    private String permissionName;
+
+    /**
+     * admin_permission
+     */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 
+     * @return id 
+     */
+    public BigInteger getId() {
+        return id;
