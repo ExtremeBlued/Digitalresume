9490d50f961baca08f469c179dc8da859cf56eb8
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2020-08-01 13:23:28 +0000

    Description added

--- common/src/main/java/com/mvc/cryptovault/common/bean/AppProjectPartake.java
+++ common/src/main/java/com/mvc/cryptovault/common/bean/AppProjectPartake.java
@@ -12,4 +12,18 @@ import java.math.BigInteger;
  * @author qiyichen
  * @create 2018/12/7 15:19
  */
-@Table(n
+@Table(name = "app_project_partake")
+@Data
+public class AppProjectPartake implements Serializable {
+
+    @Id
+    private BigInteger id;
+    private BigInteger projectId;
+    private BigInteger userId;
+    private BigDecimal value;
+    private Integer times;
+    private BigDecimal reverseValue;
+    private Long publishTime;
+    private BigInteger tokenId;
+
+}
