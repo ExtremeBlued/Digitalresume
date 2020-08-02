e7b903308af62dfd9aaaf805015dbcb2bf6077b7
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2020-08-02 06:50:34 +0000

    Update conda.rst

--- common/src/main/java/com/mvc/cryptovault/common/bean/AppProjectUserTransaction.java
+++ common/src/main/java/com/mvc/cryptovault/common/bean/AppProjectUserTransaction.java
@@ -7,4 +7,40 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import java.io.Serializable;
 import java.math.BigDecimal;
-import java.math.Big
+import java.math.BigInteger;
+
+/**
+ * app_project_user_transaction
+ */
+@Table(name = "app_project_user_transaction")
+@Data
+public class AppProjectUserTransaction implements Serializable {
+    /**
+     * 
+     */
+    @Id
+    @Column(name = "id")
+    private BigInteger id;
+
+    /**
+     * 用户id
+     */
+    @Column(name = "user_id")
+    private BigInteger userId;
+
+    /**
+     * 项目id
+     */
+    @Column(name = "project_id")
+    private BigInteger projectId;
+
+    /**
+     * 创建时间
+     */
+    @Column(name = "created_at")
+    private Long createdAt;
+
+    /**
+     * 修改时间
+     */
+    @Column(name = "updated
