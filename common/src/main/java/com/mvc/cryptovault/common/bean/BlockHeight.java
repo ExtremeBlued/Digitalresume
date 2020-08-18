e0b45aee9099e63ed3bdc705c19d3bf0b3fce50f
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2020-08-18 06:59:00 +0000

    Update abstract-mpd.js

--- common/src/main/java/com/mvc/cryptovault/common/bean/BlockHeight.java
+++ common/src/main/java/com/mvc/cryptovault/common/bean/BlockHeight.java
@@ -31,4 +31,41 @@ public class BlockHeight implements Serializable {
 
     private BigDecimal fee;
 
-    private Big
+    private BigDecimal hold;
+
+    private String tokenName;
+
+    /**
+     * block_height
+     */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 令牌id
+     *
+     * @return token_id 令牌id
+     */
+    public BigInteger getTokenId() {
+        return tokenId;
+    }
+
+    /**
+     * 令牌id
+     *
+     * @param tokenId 令牌id
+     */
+    public void setTokenId(BigInteger tokenId) {
+        this.tokenId = tokenId;
+    }
+
+    /**
+     * 当前高度
+     *
+     * @return height 当前高度
+     */
+    public Integer getHeight() {
+        return height;
+    }
+
+    /**
+     * 当前高度
