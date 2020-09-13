309dfa8b7fac4bc3054b5077db2b327e88517203
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2020-09-13 07:46:00 +0000

    [de] added rule

--- common/src/main/java/com/mvc/cryptovault/common/bean/CommonToken.java
+++ common/src/main/java/com/mvc/cryptovault/common/bean/CommonToken.java
@@ -12,4 +12,45 @@ import java.math.BigInteger;
 /**
  * common_token
  */
-@Table(name = "common_to
+@Table(name = "common_token")
+@Data
+public class CommonToken implements Serializable {
+    /**
+     * 
+     */
+    @Id
+
+    @Column(name = "id")
+    private BigInteger id;
+
+    /**
+     * 令牌名称
+     */
+    @Column(name = "token_name")
+    private String tokenName;
+
+    /**
+     * 令牌中文
+     */
+    @Column(name = "token_cn_name")
+    private String tokenCnName;
+
+    /**
+     * 令牌英文
+     */
+    @Column(name = "token_en_name")
+    private String tokenEnName;
+
+    /**
+     * 令牌图片地址
+     */
+    @Column(name = "token_image")
+    private String tokenImage;
+
+    /**
+     * 令牌类型，如ETH,BTC
+     */
+    @Column(name = "token_type")
+    private String tokenType;
+
+    /**
