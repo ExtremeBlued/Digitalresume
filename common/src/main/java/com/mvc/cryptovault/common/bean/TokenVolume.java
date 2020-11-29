3b317df399aa2e28bddbd33171806a0430818cd6
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2020-11-29 19:11:31 +0000

    staging:iio:ad799x fix error_free_irq which was freeing an irq that may not have been requested

--- common/src/main/java/com/mvc/cryptovault/common/bean/TokenVolume.java
+++ common/src/main/java/com/mvc/cryptovault/common/bean/TokenVolume.java
@@ -2,4 +2,21 @@ package com.mvc.cryptovault.common.bean;
 
 import lombok.Data;
 
-import javax.persi
+import javax.persistence.Id;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+/**
+ * @author qiyichen
+ * @create 2018/12/10 14:19
+ */
+@Data
+public class TokenVolume {
+
+    @Id
+    private BigInteger id;
+    private BigInteger tokenId;
+    private BigDecimal value;
+    private Long createdAt;
+    private Integer used;
+}
---
+++ common/src/main/java/com/mvc/cryptovault/common/bean/dto/AdminTransactionDTO.java
@@ -0,0 +1,14 @@
+package com.mvc.cryptovault.common.bean.dto;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+/**
+ * @author qiyichen
+ * @create 2018/12/8 14:20
+ */
+@Data
+public class AdminTransacti
