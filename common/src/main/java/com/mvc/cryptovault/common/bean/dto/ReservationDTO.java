f9a8d8d6551bbc2ecc09d914877115d223edb88e
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2020-12-13 09:20:49 +0000

    urgh

---
+++ common/src/main/java/com/mvc/cryptovault/common/bean/dto/ReservationDTO.java
@@ -0,0 +1,11 @@
+package com.mvc.cryptovault.common.bean.dto;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.math.BigInteger;
+
+/**
+ * @author qiyichen
+ * @create 2018
