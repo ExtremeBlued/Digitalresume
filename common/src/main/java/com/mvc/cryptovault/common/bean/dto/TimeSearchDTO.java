bee95a287541c71a7c944e059866cf177131bdb9
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2020-12-13 09:41:50 +0000

    0.5.14

---
+++ common/src/main/java/com/mvc/cryptovault/common/bean/dto/TimeSearchDTO.java
@@ -0,0 +1,17 @@
+package com.mvc.cryptovault.common.bean.dto;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.math.BigInteger;
+
+/**
+ * @author qiyichen
+ * @create 2018/11/13 15:47
+ */
+@Data
+public class TimeSearchDTO {
+
+    private BigInteger timestamp;
+    @ApiModelProperty("0上拉 1下拉")
+   
