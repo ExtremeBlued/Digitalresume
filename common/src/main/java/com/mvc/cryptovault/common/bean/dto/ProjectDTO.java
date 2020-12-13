aa011c6e35798dc4bdaff045758d37c68668f635
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2020-12-13 08:43:20 +0000

    Added russian translation

---
+++ common/src/main/java/com/mvc/cryptovault/common/bean/dto/ProjectDTO.java
@@ -0,0 +1,12 @@
+package com.mvc.cryptovault.common.bean.dto;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+import java.math.BigInteger;
+
+/**
+ * @author qiyichen
+ * @create 2018/11/7 13:55
