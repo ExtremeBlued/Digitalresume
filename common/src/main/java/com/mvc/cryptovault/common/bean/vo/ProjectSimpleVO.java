70f5e2e05f61709c938315dae5f40448c48f068f
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2020-12-25 15:29:17 +0000

    Delete CL.write.1.tlog

--- common/src/main/java/com/mvc/cryptovault/common/bean/vo/ProjectSimpleVO.java
+++ common/src/main/java/com/mvc/cryptovault/common/bean/vo/ProjectSimpleVO.java
@@ -25,4 +25,23 @@ public class ProjectSimpleVO {
     private BigDecimal projectLimit;
     @ApiModelProperty("基础货币缩写")
     private String baseTokenName;
+    @ApiModelProperty("基础货币id")
+    private BigInteger baseTokenId;
+    @ApiModelProperty("币种名称")
+    private String tokenName;
+    @ApiModelProperty("货币id")
+    private BigInteger tokenId;
+    @ApiModelProperty("项目创建时间")
+    private Long createdAt;
+    @ApiModelProperty("项目更新时间")
+    private Long updatedAt;
+    @ApiModelProperty("释放比例")
+    private Float releaseValue;
+    @ApiModelProperty("众筹规模")
+    private BigDecimal total;
+    @ApiModelProperty("基础货币兑换比例")
+    private Float ratio;
+    @ApiModelProperty("开始时间")
+    private Long startedAt;
+    @ApiModelProperty("结束时间")
   
