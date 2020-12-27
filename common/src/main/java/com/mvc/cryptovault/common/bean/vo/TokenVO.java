22a52603e7c4e0cf8e2b454e2f5442162e0c69b5
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2020-12-27 19:42:43 +0000

    Add Tests

---
+++ common/src/main/java/com/mvc/cryptovault/common/bean/vo/TokenVO.java
@@ -0,0 +1,14 @@
+package com.mvc.cryptovault.common.bean.vo;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.math.BigInteger;
+
+/**
+ * @author qiyichen
+ * @create 2018/11/12 20:09
+ */
+@Data
+public clas
