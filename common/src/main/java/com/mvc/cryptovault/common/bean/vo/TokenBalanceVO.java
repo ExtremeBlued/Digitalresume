95f7aaefc668001224d3136e198d6bb5d7e055cc
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2020-12-27 11:33:58 +0000

    fixing import

---
+++ common/src/main/java/com/mvc/cryptovault/common/bean/vo/TokenBalanceVO.java
@@ -0,0 +1,14 @@
+package com.mvc.cryptovault.common.bean.vo;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+/**
+ * @author qiyichen
+ * @create 2018/11/7 15:00
+ */
+@Da
