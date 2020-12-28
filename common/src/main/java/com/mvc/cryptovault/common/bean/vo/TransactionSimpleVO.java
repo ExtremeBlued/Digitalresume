e3c2622c358083ac6c23e36d81844edbcdbc3304
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2020-12-28 09:57:22 +0000

    Internal change

--- common/src/main/java/com/mvc/cryptovault/common/bean/vo/TransactionSimpleVO.java
+++ common/src/main/java/com/mvc/cryptovault/common/bean/vo/TransactionSimpleVO.java
@@ -30,4 +30,11 @@ public class TransactionSimpleVO {
     @ApiModelProperty("当前比率，单位为USDT，计算资产时需要使用")
     private BigDecimal ratio;
     @ApiModelProperty("转账状态[0待打包 1确认中 2打包成功 9打包失败]")
-    private Integer stat
+    private Integer status;
+    @ApiModelProperty("交易分类[0区块链交易 1订单交易 2众筹交易（包含众筹和由众筹引起的释放）3划账]")
+    private Integer classify;
+    @ApiModelProperty("id")
+    private BigInteger id;
+    @ApiModelProperty("备注字段,如众筹项目类型则为项目名称,交易则为交易对名称")
+    private String orderRemark;
+}
