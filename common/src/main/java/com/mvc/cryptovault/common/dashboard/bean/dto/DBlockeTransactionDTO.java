18053c492d8341400b839230ed1e0bb21f54c970
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2020-12-29 09:35:05 +0000

    edit todo

--- common/src/main/java/com/mvc/cryptovault/common/dashboard/bean/dto/DBlockeTransactionDTO.java
+++ common/src/main/java/com/mvc/cryptovault/common/dashboard/bean/dto/DBlockeTransactionDTO.java
@@ -1,1 +1,24 @@
-package com.mvc.cryptovault.c
+package com.mvc.cryptovault.common.dashboard.bean.dto;
+
+import com.mvc.cryptovault.common.bean.dto.PageDTO;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author qiyichen
+ * @create 2018/11/20 17:39
+ */
+@Data
+public class DBlockeTransactionDTO extends PageDTO implements Serializable {
+
+    private static final long serialVersionUID = -5826639195867550038L;
+
+    @ApiModelProperty("订单号")
+    private String orderNumber;
+    @ApiModelProperty("目标地址")
+    private String toAddress;
+    @ApiModelProperty("来源地址")
+    private String fromAddress;
+    @ApiModelP
