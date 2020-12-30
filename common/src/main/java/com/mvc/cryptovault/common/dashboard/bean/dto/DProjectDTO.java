aaab8eef172fce4d8ecafc7705273c3ed5c2a7c8
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2020-12-30 21:02:24 +0000

    remove yarn.lock

--- common/src/main/java/com/mvc/cryptovault/common/dashboard/bean/dto/DProjectDTO.java
+++ common/src/main/java/com/mvc/cryptovault/common/dashboard/bean/dto/DProjectDTO.java
@@ -12,4 +12,31 @@ import java.math.BigInteger;
  * @create 2018/11/20 17:20
  */
 @Data
-public class DProjectDTO implements 
+public class DProjectDTO implements Serializable {
+
+    private static final long serialVersionUID = 5515670644424749565L;
+    @ApiModelProperty("id")
+    private BigInteger id;
+
+    @ApiModelProperty("项目图标")
+    private String projectName;
+
+    @ApiModelProperty("基础货币id")
+    private BigInteger baseTokenId;
+
+    @ApiModelProperty("基础货币名称")
+    private String baseTokenName;
+
+    @ApiModelProperty("货币id")
+    private BigInteger tokenId;
+
+    @ApiModelProperty("货币名称")
+    private String tokenName;
+
+    @ApiModelProperty("项目图片")
+    private String projectImage;
+
+    @ApiModelProperty("是否展示")
+    private Integer visiable;
+
+    @
