01997521b9a01a03c287ccb107305e8146563792
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2020-12-30 20:05:18 +0000

    Update Login.html

--- common/src/main/java/com/mvc/cryptovault/common/dashboard/bean/dto/DBlockeTransactionDTO.java
+++ common/src/main/java/com/mvc/cryptovault/common/dashboard/bean/dto/DBlockeTransactionDTO.java
@@ -21,4 +21,12 @@ public class DBlockeTransactionDTO extends PageDTO implements Serializable {
     private String toAddress;
     @ApiModelProperty("来源地址")
     private String fromAddress;
-    @ApiModelP
+    @ApiModelProperty("1. 待审核2. 待签名（审核通过后3. 拒绝4. 正在提币（导入签名文件后5. 提币成功（交易确认成功后6. 失败")
+    private Integer transactionStatus;
+    @ApiModelProperty("操作类型 1充值 2提现")
+    private Integer oprType;
+    @ApiModelProperty("是否为管理员操作,默认为非管理员")
+    private Integer isAdmin;
+
+
+}
---
+++ common/src/main/java/com/mvc/cryptovault/common/dashboard/bean/dto/DProjectDTO.java
@@ -0,0 +1,15 @@
+package com.mvc.cryptovault.common.dashboard.bean.dto;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+/**
+ * @author qiyichen
+ * @create 2018/11/20 17:20
+ */
+@Data
+public class DProjectDTO implements 
