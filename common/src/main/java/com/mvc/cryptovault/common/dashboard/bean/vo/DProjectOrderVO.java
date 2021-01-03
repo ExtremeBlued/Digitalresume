f1f1152832ced0b060b86e29c43be615e95a59f7
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2021-01-03 12:36:43 +0000

    Small refactor

--- common/src/main/java/com/mvc/cryptovault/common/dashboard/bean/vo/DProjectOrderVO.java
+++ common/src/main/java/com/mvc/cryptovault/common/dashboard/bean/vo/DProjectOrderVO.java
@@ -1,1 +1,28 @@
-package com.mvc.cryptovault.common.dashboard.bean.vo
+package com.mvc.cryptovault.common.dashboard.bean.vo;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+/**
+ * @author qiyichen
+ * @create 2018/11/20 17:23
+ */
+@Data
+public class DProjectOrderVO implements Serializable {
+
+    private static final long serialVersionUID = -2959375044042658750L;
+    @ApiModelProperty("创建时间")
+    private Long createdAt;
+    @ApiModelProperty("订单号")
+    private String orderNumber;
+    @ApiModelProperty("项目名称")
+    private String projectName;
+    @ApiModelProperty("项目id")
+    private String projectId;
+    @ApiModelProperty("用户手机号")
+    private String cellphone;
+   
