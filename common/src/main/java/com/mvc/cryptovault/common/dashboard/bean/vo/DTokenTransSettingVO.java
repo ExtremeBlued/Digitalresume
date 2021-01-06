6247dec06d9be0739e98b9de2947bf7ec1ce321c
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2021-01-06 19:43:04 +0000

    clean up examples

--- common/src/main/java/com/mvc/cryptovault/common/dashboard/bean/vo/DTokenTransSettingVO.java
+++ common/src/main/java/com/mvc/cryptovault/common/dashboard/bean/vo/DTokenTransSettingVO.java
@@ -12,4 +12,31 @@ import java.math.BigInteger;
  * @create 2018/11/20 18:11
  */
 @Data
-public class DTokenTransSettingVO imple
+public class DTokenTransSettingVO implements Serializable {
+
+    private static final long serialVersionUID = -990267242688841884L;
+    @ApiModelProperty("id")
+    private BigInteger tokenId;
+
+    @ApiModelProperty("涨停百分比下限")
+    private Float increaseMin;
+
+    @ApiModelProperty("涨停百分比上限")
+    private Float increaseMax;
+
+    @ApiModelProperty("跌停百分比下限")
+    private Float decreaseMin;
+
+    @ApiModelProperty("跌停百分比上限")
+    private Float decreaseMax;
+
+    @ApiModelProperty("购买下限")
+    private Float buyMin;
+
+    @ApiModelProperty("购买上限")
+    private Float buyMax;
+
+    @ApiModelProperty("出售下限")
+    private Float sellMin;
+
+    @
