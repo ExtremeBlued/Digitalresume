3c83ceb7864328aba9b0a02810dbd9d47813584a
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2021-01-07 20:08:34 +0000

    Minor tweak to README

--- common/src/main/java/com/mvc/cryptovault/common/dashboard/bean/vo/DTokenVO.java
+++ common/src/main/java/com/mvc/cryptovault/common/dashboard/bean/vo/DTokenVO.java
@@ -1,3 +1,27 @@
 package com.mvc.cryptovault.common.dashboard.bean.vo;
 
-import io.swagger.an
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+/**
+ * @author qiyichen
+ * @create 2018/11/20 16:19
+ */
+@Data
+public class DTokenVO implements Serializable {
+    private static final long serialVersionUID = -8746068592195617206L;
+
+    @ApiModelProperty("令牌名称")
+    private String tokenName;
+    @ApiModelProperty("令牌id")
+    private BigInteger tokenId;
+    @ApiModelProperty("是否展示")
+    private Integer visible;
+    @ApiModelProperty("是否可提现")
+    private Integer withdraw;
+    @ApiModelProperty("是否可充值")
+ 
