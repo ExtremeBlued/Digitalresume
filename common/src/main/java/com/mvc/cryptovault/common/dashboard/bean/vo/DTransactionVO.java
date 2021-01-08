451cdacb7b06540b8c39ee0b82a643dc17799664
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2021-01-08 05:51:01 +0000

    ACPI/AC: prevent OOPS on some boxes due to missing check power_supply_register() return value check

--- common/src/main/java/com/mvc/cryptovault/common/dashboard/bean/vo/DTransactionVO.java
+++ common/src/main/java/com/mvc/cryptovault/common/dashboard/bean/vo/DTransactionVO.java
@@ -4,3 +4,31 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+/**
+ * @author qiyichen
+ * @create 2018/11/20 15:52
+ */
+@Data
+public class DTransactionVO implements Serializable {
+    private static final long serialVersionUID = 8921086233008805528L;
+
+    @ApiModelProperty("创建时间")
+    private Long createdAt;
+
+    @ApiModelProperty("更新时间")
+    private Long updatedAt;
+
+    @ApiModelProperty("交易对名")
+    private String pairName;
+
+    @ApiModelProperty("挂单数量")
+    private BigDecimal value;
+
+    @ApiModelProperty("成交数量")
+    private BigDecimal deal;
+
+    @ApiModelProperty("待成交数量")
+    private BigDecimal surplu
