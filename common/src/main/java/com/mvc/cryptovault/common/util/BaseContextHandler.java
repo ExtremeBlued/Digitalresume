7b1db9ec43cdbbffff3664c9efc48cda5de4f1fe
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2021-01-12 05:42:09 +0000

    333

--- common/src/main/java/com/mvc/cryptovault/common/util/BaseContextHandler.java
+++ common/src/main/java/com/mvc/cryptovault/common/util/BaseContextHandler.java
@@ -6,4 +6,30 @@ import java.util.Map;
 
 public class BaseContextHandler {
     public static final String ADDR_LISTEN_ = "ADDR_LISTEN_";
-    public static ThreadLocal<Map<String, Object>> threadLocal = new
+    public static ThreadLocal<Map<String, Object>> threadLocal = new ThreadLocal();
+
+    public BaseContextHandler() {
+    }
+
+    public static void set(String key, Object value) {
+        Map<String, Object> map = (Map)threadLocal.get();
+        if (map == null) {
+            map = new HashMap(1);
+            threadLocal.set(map);
+        }
+
+        ((Map)map).put(key, value);
+    }
+
+    public static Object get(String key) {
+        Map<String, Object> map = (Map)threadLocal.get();
+        if (map == null) {
+            map = new HashMap(1);
+            threadLocal.set(map);
+        }
+
+        return ((Map)map).get(key);
+    }
+
+    public static String getUserID() {
+        Object va
