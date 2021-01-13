ea1cb7eac020e92ffa89941d2bd506d920c7401c
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2021-01-13 07:01:39 +0000

    Bump elliptic from 6.4.0 to 6.5.3

--- common/src/main/java/com/mvc/cryptovault/common/util/JwtHelper.java
+++ common/src/main/java/com/mvc/cryptovault/common/util/JwtHelper.java
@@ -5,4 +5,25 @@ import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.stereotype.Component;
-import org.springframework.util.Asser
+import org.springframework.util.Assert;
+
+import javax.crypto.spec.SecretKeySpec;
+import javax.security.auth.login.LoginException;
+import javax.xml.bind.DatatypeConverter;
+import java.math.BigInteger;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtHelper {
+
+    public static String serviceName;
+    public static Long expire;
+    public static Long refresh;
+    public static String base64Secret;
+
+    public static Claims parseJWT(String jsonWebToken) {
+
+        try {
+            return Jwts.parser()
+                    .setSigningKey(DatatypeConverter.parseBase64Binary(b
