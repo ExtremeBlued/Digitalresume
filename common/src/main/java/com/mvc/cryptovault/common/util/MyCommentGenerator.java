54387f7983f368e02b0aa543881e6a881f1eb581
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2021-01-17 12:18:43 +0000

    Update doc strings

---
+++ common/src/main/java/com/mvc/cryptovault/common/util/MyCommentGenerator.java
@@ -0,0 +1,74 @@
+
+package com.mvc.cryptovault.common.util;
+
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.dom.java.*;
+import org.mybatis.generator.api.dom.xml.XmlElement;
+import org.mybatis.generator.config.MergeConstants;
+import org.mybatis.generator.config.PropertyRegistry;
+import org.mybatis.generator.internal.DefaultCommentGenerator;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Properties;
+import java.util.Set;
+
+import static org.mybatis.generator.internal.util.StringUtility.isTrue;
+
+/**
+ * mybatis generator 自定义comment生成器.
+ *
+ * @author yihui
+ *
+ */
+public class MyCommentGenerator extends DefaultCommentGenerator {// implements
+    // CommentGenerator
+    // {
+
+    private Properties properties;
+    private Properties systemPro;
+    private boolean suppressDate;
+    private boolean suppressAllComments;
+    private String currentDateStr;
+
+    public MyCommentGenerator() {
+        super();
+        properties = new Properties();
+        systemPro = System.getProperties();
+        suppressDate = false;
+        suppressAllComments = false;
+        currentDateStr = (new SimpleDateFormat("yyyy-MM-dd")).format(new Date());
+    }
+
+    public void addJavaFileComment(CompilationUnit compilationUnit) {
+        // add no file level comments by default
+        return;
+    }
+
+    /**
+     * Adds a suitable comment to warn users that the element was generated, and
+     * when it was generated.
+     */
+    public void addComment(XmlElement xmlElement) {
+        return;
+    }
+
+    public void addRootComment(XmlElement rootElement) {
+        // add no document level comments by default
+        return;
+    }
+
+    public void addConfigurationProperties(Properties properties) {
+        this.properties.putAll(properties);
+
+        suppressDate = isTrue(properties.getProperty(PropertyRegistry.COMMENT_GENERATOR_SUPPRESS_DATE));
+
+        suppressAllComments = isTrue(properties.getProperty(PropertyRegistry.COMMENT_GENERATOR_SUPPRESS_ALL_COMMENTS));
+    }
+
+    /**
+     * This method adds the custom javadoc tag for. You may do nothing if you do
+     * not wish to include the Javadoc tag - however, if you do not include the
+     * Javadoc tag then the Java merge capability of the eclipse plugin will
