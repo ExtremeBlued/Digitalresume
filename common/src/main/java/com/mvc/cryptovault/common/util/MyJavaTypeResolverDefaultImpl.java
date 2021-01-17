dd6d6ba3b7c0c345c8caf5ec231f551c28b021b8
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2021-01-17 19:00:33 +0000

    Small refactoring.

--- common/src/main/java/com/mvc/cryptovault/common/util/MyJavaTypeResolverDefaultImpl.java
+++ common/src/main/java/com/mvc/cryptovault/common/util/MyJavaTypeResolverDefaultImpl.java
@@ -9,4 +9,12 @@ import java.sql.Types;
 
 public class MyJavaTypeResolverDefaultImpl extends JavaTypeResolverDefaultImpl {
 
-    public MyJavaTypeResolverDe
+    public MyJavaTypeResolverDefaultImpl() {
+        super();
+        //把数据库的 TINYINT 映射成 Integer
+        super.typeMap.put(Types.TINYINT, new JdbcTypeInformation("TINYINT", new FullyQualifiedJavaType(Integer.class.getName())));
+        super.typeMap.put(Types.SMALLINT, new JdbcTypeInformation("SMALLINT", new FullyQualifiedJavaType(Integer.class.getName())));
+        super.typeMap.put(Types.BIGINT, new JdbcTypeInformation("BIGINT", new FullyQualifiedJavaType(BigInteger.class.getName())));
+        super.typeMap.put(Types.DECIMAL, new JdbcTypeInformation("DECIMAL", new FullyQualifiedJavaType(BigDecimal.class.getName())));
+    }
+}
