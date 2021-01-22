dcdc903dcca4237dac4b0ab0d9e3c30b22ab3d27
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2021-01-22 13:32:42 +0000

    Add editor config

---
+++ console/src/main/java/com/mvc/cryptovault/console/config/BeanConfig.java
@@ -0,0 +1,7 @@
+package com.mvc.cryptovault.console.config;
+
+import cn.jiguang.common.ClientConfig;
+import cn.jpush.api.JPushClient;
+import com.mvc.cryptovault.common.util.JwtHelper;
+import com.mvc.cryptovault.console.util.btc.BtcAction;
+import com.neemre.btcdcli4j.core.BitcoindExce
