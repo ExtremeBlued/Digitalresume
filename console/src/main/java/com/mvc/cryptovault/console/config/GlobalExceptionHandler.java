087eabcb626e43c8aa84d7f7148f7a8ff48b0f6f
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2021-01-26 22:28:13 +0000

    Chapter 4

--- console/src/main/java/com/mvc/cryptovault/console/config/GlobalExceptionHandler.java
+++ console/src/main/java/com/mvc/cryptovault/console/config/GlobalExceptionHandler.java
@@ -8,4 +8,22 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import javax.
+import javax.security.auth.login.LoginException;
+
+/**
+ * @author qyc
+ */
+@ControllerAdvice
+@ResponseBody
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(LoginException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public Result loginExceptionException() {
+        return new Result(HttpStatus.FORBIDDEN.value(), "token error", null);
+    }
+
+    @ExceptionHandler(TokenErrorException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public Result tokenErrorExceptionException() {
+        return new Result(HttpStatus.FORBIDDEN.value(), "token
