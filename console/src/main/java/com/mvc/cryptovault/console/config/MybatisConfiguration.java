30fb57587edb060137abeab32bc015abfaa60022
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2021-01-27 19:12:18 +0000

    rescan bindings

--- console/src/main/java/com/mvc/cryptovault/console/config/MybatisConfiguration.java
+++ console/src/main/java/com/mvc/cryptovault/console/config/MybatisConfiguration.java
@@ -9,4 +9,27 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+/**
+ * mybatis 配置数据源类
+ *
+ * @author qyc
+ */
+@Configuration
+@EnableTransactionManagement
+public class MybatisConfiguration implements EnvironmentAware {
+
+    private String driveClassName;
+    private String url;
+    private String userName;
+    private String password;
+    private String xmlLocation;
+    private String typeAliasesPackage;
+    private String filters;
+    private S
