ebf95676115977b1956e0e7fa1364ffa8f8fe072
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2021-02-01 16:33:54 +0000

    timer: Generalize timer->base flags handling

--- console/src/main/java/com/mvc/cryptovault/console/config/SpringContextUtil.java
+++ console/src/main/java/com/mvc/cryptovault/console/config/SpringContextUtil.java
@@ -6,4 +6,19 @@ import org.springframework.beans.factory.BeanFactoryAware;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class SpringContext
+public class SpringContextUtil implements BeanFactoryAware {
+    private static BeanFactory beanFactory;
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        SpringContextUtil.beanFactory = beanFactory;
+    }
+
+    public static <T> T getBean(String beanName) {
+        if (null != beanFactory) {
+            return (T) beanFactory.getBean(beanName);
+        }
+        return null;
+    }
+
+}
---
+++ console/src/main/java/com/mvc/cryptovault/console/constant/BusinessConstant.java
@@ -0,0 +1,12 @@
+package com.mvc.cryptovault.console.constant;
+
+import java.math.BigInteger;
+
+public interface BusinessConstant {
+
+    /**
+     * 上拉
+     */
+    Integer SEARCH_DIRECTION_UP = 0;
+    /**
+     
