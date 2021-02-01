6a016980bc8c981ae822c75a38c376f4d4e3821f
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2021-02-01 08:29:35 +0000

    Delete bg.jpg

--- console/src/main/java/com/mvc/cryptovault/console/config/ServiceAuthRestInterceptor.java
+++ console/src/main/java/com/mvc/cryptovault/console/config/ServiceAuthRestInterceptor.java
@@ -1,4 +1,19 @@
 package com.mvc.cryptovault.console.config;
 
 import com.mvc.cryptovault.common.util.BaseContextHandler;
-import org.springframework.context.i18
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author qyc
+ */
+public class ServiceAuthRestInterceptor extends HandlerInterceptorAdapter {
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        BaseContextHandler.remove();
+        super.afterCompletion(request, response, handler, ex
