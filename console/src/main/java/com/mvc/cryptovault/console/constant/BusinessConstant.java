6b100762767a26c14631d409f11e3606715f44b4
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2021-02-02 11:35:58 +0000

    Fixes #58

--- console/src/main/java/com/mvc/cryptovault/console/constant/BusinessConstant.java
+++ console/src/main/java/com/mvc/cryptovault/console/constant/BusinessConstant.java
@@ -40,4 +40,35 @@ public interface BusinessConstant {
     BigInteger BASE_TOKEN_ID_USDT = BigInteger.valueOf(4);
     /**
      * 购买
-   
+     */
+    Integer TRANSACTION_TYPE_BUY = 1;
+    /**
+     * 出售
+     */
+    Integer TRANSACTION_TYPE_SELL = 2;
+    /**
+     * 充值
+     */
+    Integer OPR_TYPE_RECHARGE= 1;
+    /**
+     * 提现
+     */
+    Integer OPR_TYPE_WITHDRAW = 2;
+    /**
+     * 取消状态
+     */
+    Integer STATUS_CANCEL = 4;
+    /**
+     * 区跨链类型
+     */
+    String CONTENT_BLOCK = "CONTENT_BLOCK";
+    String CONTENT_PROJECT = "CONTENT_PROJECT";
+    String CONTENT_TRANSACTION = "CONTENT_TRANSACTION";
+    String CONTENT_EMIT = "CONTENT_EMIT";
+
+    /**
+     * 区跨链类型
+     */
+    Integer CLASSIFY_BLOCK = 0;
+    String REDIS_QUEUE = "REDIS_QUEUE";
+    String WITHDRAW_USDT_QUEUE = "WITH
