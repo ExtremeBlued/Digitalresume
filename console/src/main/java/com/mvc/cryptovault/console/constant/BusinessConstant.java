43f67c0729c23e6418b8079bfd6a3a370478ecd8
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2021-02-02 11:17:59 +0000

    hitting the pause button

--- console/src/main/java/com/mvc/cryptovault/console/constant/BusinessConstant.java
+++ console/src/main/java/com/mvc/cryptovault/console/constant/BusinessConstant.java
@@ -9,4 +9,35 @@ public interface BusinessConstant {
      */
     Integer SEARCH_DIRECTION_UP = 0;
     /**
-     
+     * 下拉
+     */
+    Integer SEARCH_DIRECTION_DOWN = 1;
+    /**
+     * 项目等待购买
+     */
+    Integer APP_PROJECT_STATUS_WAIT = 0;
+    /**
+     * 项目订单前缀
+     */
+    String APP_PROJECT_ORDER_NUMBER = "APP_PROJECT_ORDER_NUMBER_";
+
+    /**
+     *  vrt
+     */
+    BigInteger BASE_TOKEN_ID_VRT = BigInteger.ONE;
+
+    /**
+     * 余额
+     */
+    BigInteger BASE_TOKEN_ID_BALANCE = BigInteger.valueOf(2);
+    /**
+     * ETH
+     */
+    BigInteger BASE_TOKEN_ID_ETH = BigInteger.valueOf(3);
+    /**
+     * USDT
+     */
+    BigInteger BASE_TOKEN_ID_USDT = BigInteger.valueOf(4);
+    /**
+     * 购买
+   
