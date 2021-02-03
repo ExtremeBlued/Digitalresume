972030b47ea540cc8f59c0f349c06a3182918e79
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2021-02-03 09:55:17 +0000

    fix table

--- console/src/main/java/com/mvc/cryptovault/console/controller/AppMessageController.java
+++ console/src/main/java/com/mvc/cryptovault/console/controller/AppMessageController.java
@@ -10,4 +10,28 @@ import com.mvc.cryptovault.console.service.AppMessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.math.BigInt
+import java.math.BigInteger;
+import java.util.List;
+
+/**
+ * @author qiyichen
+ * @create 2018/11/13 16:02
+ */
+@RestController
+@RequestMapping("appMessage")
+public class AppMessageController extends BaseController {
+
+    @Autowired
+    AppMessageService appMessageService;
+
+    /**
+     * TODO 根据性能结果缓存或走搜索引擎
+     *
+     * @param userId
+     * @param timeSearchDTO
+     * @param pageDTO
+     * @return
+     */
+    @GetMapping()
+    public Result<PageInfo<AppMessage>> list(@RequestParam BigInteger userId, @ModelAttribute TimeSearchDTO timeSearchDTO, @ModelAttribute PageDTO pageDTO) {
+        List<AppMessage> message = appMessag
