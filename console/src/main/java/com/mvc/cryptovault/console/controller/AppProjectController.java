ce851dab0ea1162a48d25113e876cb1758837bec
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2021-02-16 11:50:10 +0000

    shave some bytes

--- console/src/main/java/com/mvc/cryptovault/console/controller/AppProjectController.java
+++ console/src/main/java/com/mvc/cryptovault/console/controller/AppProjectController.java
@@ -10,4 +10,22 @@ import com.mvc.cryptovault.console.service.AppProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import 
+import java.math.BigInteger;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author qiyichen
+ * @create 2018/11/13 17:05
+ */
+@RestController
+@RequestMapping("appProject")
+public class AppProjectController extends BaseController {
+
+    @Autowired
+    AppProjectService appProjectService;
+
+    @GetMapping()
+    Result<PageInfo<AppProject>> getProject(@RequestParam Integer projectType, @RequestParam(required = false) BigInteger id, @RequestParam Integer type, @RequestParam Integer pageSize) {
+        final BigInteger projectId = id == null ? BigInteger.ZERO : id;
+        PageHelper.order
