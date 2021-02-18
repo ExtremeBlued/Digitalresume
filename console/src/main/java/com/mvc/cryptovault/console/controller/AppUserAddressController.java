64aefafae1bfa9542668ee739fd686ad5756f1df
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2021-02-18 20:47:58 +0000

    Add cleanup

--- console/src/main/java/com/mvc/cryptovault/console/controller/AppUserAddressController.java
+++ console/src/main/java/com/mvc/cryptovault/console/controller/AppUserAddressController.java
@@ -2,4 +2,23 @@ package com.mvc.cryptovault.console.controller;
 
 import com.mvc.cryptovault.common.bean.vo.Result;
 import com.mvc.cryptovault.console.common.BaseController;
-import com.mvc.cryptovault.console.se
+import com.mvc.cryptovault.console.service.AppUserAddressService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigInteger;
+
+/**
+ * @author qiyichen
+ * @create 2018/11/14 14:40
+ */
+@RestController
+@RequestMapping("appUserAddress")
+public class AppUserAddressController extends BaseController {
+
+    @Autowired
+    AppUserAddressService appUserAddressService;
+
+    @GetMapping("{userId}")
+    public Result<String> getAddress(@PathVariable("userId") BigInteger userId, @RequestParam("tokenId") BigInteger tokenId) {
+        String address = appUserAddressService.
