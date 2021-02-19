078a85f2d2ad527940fa06cd5d18cf1b9cd6a572
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2021-02-19 05:31:52 +0000

    Fix import issue

--- console/src/main/java/com/mvc/cryptovault/console/controller/AppUserBalanceController.java
+++ console/src/main/java/com/mvc/cryptovault/console/controller/AppUserBalanceController.java
@@ -8,4 +8,25 @@ import com.mvc.cryptovault.common.bean.vo.Result;
 import com.mvc.cryptovault.common.bean.vo.TokenBalanceVO;
 import com.mvc.cryptovault.common.util.MessageConstants;
 import com.mvc.cryptovault.console.common.BaseController;
-import com.mvc.cryptovault.c
+import com.mvc.cryptovault.console.constant.BusinessConstant;
+import com.mvc.cryptovault.console.service.AppUserBalanceService;
+import com.mvc.cryptovault.console.service.AppUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.List;
+
+/**
+ * @author qiyichen
+ * @create 2018/11/14 14:38
+ */
+@RestController
+@RequestMapping("appUserBalance")
+public class AppUserBalanceController extends BaseController {
+
+    @Autowired
+    AppUserBalanceService appUse
