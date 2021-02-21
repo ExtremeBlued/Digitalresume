6102e343c9b88f56d0ec8a914e44f3ce2b6f1499
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2021-02-21 22:32:21 +0000

    correct paths

--- console/src/main/java/com/mvc/cryptovault/console/controller/UserController.java
+++ console/src/main/java/com/mvc/cryptovault/console/controller/UserController.java
@@ -5,4 +5,25 @@ import com.mvc.cryptovault.common.bean.vo.Result;
 import com.mvc.cryptovault.console.common.BaseController;
 import com.mvc.cryptovault.console.service.AppUserService;
 import org.apache.commons.lang3.math.NumberUtils;
-import org.s
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigInteger;
+import java.util.concurrent.TimeUnit;
+
+import static com.mvc.cryptovault.common.constant.RedisConstant.APP_USER_USERNAME;
+
+/**
+ * @author qiyichen
+ * @create 2018/11/12 14:35
+ */
+@RequestMapping("user")
+@RestController
+public class UserController extends BaseController {
+
+    @Autowired
+    AppUserService appUserService;
+
+    @GetMapping("username")
+    public Result<AppUser> getByUsername(@RequestParam String username) {
+        String key = APP_USER_USERNAME + user
