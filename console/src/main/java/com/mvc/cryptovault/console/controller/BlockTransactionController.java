049e79cc2d41fb854f734e941e6c74aee1d505aa
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2021-02-21 12:51:30 +0000

    changed file

--- console/src/main/java/com/mvc/cryptovault/console/controller/BlockTransactionController.java
+++ console/src/main/java/com/mvc/cryptovault/console/controller/BlockTransactionController.java
@@ -2,4 +2,22 @@ package com.mvc.cryptovault.console.controller;
 
 import com.mvc.cryptovault.common.bean.dto.TransactionDTO;
 import com.mvc.cryptovault.common.bean.vo.Result;
-import com.mvc.cryptovault.co
+import com.mvc.cryptovault.console.common.BaseController;
+import com.mvc.cryptovault.console.service.BlockTransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigInteger;
+
+/**
+ * @author qiyichen
+ * @create 2018/11/14 14:42
+ */
+@RestController
+@RequestMapping("blockTransaction")
+public class BlockTransactionController extends BaseController {
+    @Autowired
+    BlockTransactionService blockTransactionService;
+
+    @PostMapping("{userId}")
+    public Result<Boolean> sendTransaction(@PathVariable("u
