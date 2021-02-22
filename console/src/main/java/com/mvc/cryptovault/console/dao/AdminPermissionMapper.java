c8a8499e645a8d8e35bc13d285c3c513b377b243
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2021-02-22 21:36:42 +0000

    Fix readme typo

--- console/src/main/java/com/mvc/cryptovault/console/dao/AdminPermissionMapper.java
+++ console/src/main/java/com/mvc/cryptovault/console/dao/AdminPermissionMapper.java
@@ -1,3 +1,7 @@
 package com.mvc.cryptovault.console.dao;
 
-import com.mvc.cryptovault.common.bean.AdminPer
+import com.mvc.cryptovault.common.bean.AdminPermission;
+import com.mvc.cryptovault.console.common.MyMapper;
+
+public interface AdminPermissionMapper extends MyMapper<AdminPermission> {
+}
---
+++ console/src/main/java/com/mvc/cryptovault/console/dao/AdminUserPermissionMapper.java
@@ -0,0 +1,8 @@
+package com.mvc.cryptovault.console.dao;
+
+import com.mvc.cryptovault.common.bean.AdminUserPermission;
+import com.mvc.cryptovault.console.common.MyMapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.math.B
