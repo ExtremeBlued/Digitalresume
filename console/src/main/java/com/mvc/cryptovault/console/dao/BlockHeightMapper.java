f3fb14eb29aec7b1124f0ceae88b099f01f0642f
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2021-02-27 22:52:20 +0000

    ceph: fix statvfs fr_size

--- console/src/main/java/com/mvc/cryptovault/console/dao/BlockHeightMapper.java
+++ console/src/main/java/com/mvc/cryptovault/console/dao/BlockHeightMapper.java
@@ -5,4 +5,14 @@ import com.mvc.cryptovault.console.common.MyMapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Update;
 
-im
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+public interface BlockHeightMapper extends MyMapper<BlockHeight> {
+
+    @Update("update block_height set hold = #{value} where token_id = #{tokenId}")
+    void updateHold(@Param("tokenId") BigInteger tokenId, @Param("value") BigDecimal value);
+
+    @Update("update block_height set fee = #{value} where token_id = #{tokenId}")
+    void updateFee(@Param("tokenId") BigInteger tokenId, @Param("value") BigDecimal value);
+}
---
+++ console/src/main/java/com/mvc/cryptovault/console/dao/BlockSignMapper.java
@@ -0,0 +1,4 @@
+package com.mvc.cryptovault.console.dao;
+
+import com.mvc.cryptovault.common.bean.BlockSign;
+import com.mvc.cryptovault.console.common.MyMapper;
