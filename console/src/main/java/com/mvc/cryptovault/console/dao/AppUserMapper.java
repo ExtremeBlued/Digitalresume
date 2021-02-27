4f20cc14784679d482fce80c8490177b90aeb188
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2021-02-27 21:23:57 +0000

    Updated comments

---
+++ console/src/main/java/com/mvc/cryptovault/console/dao/AppUserMapper.java
@@ -0,0 +1,8 @@
+
+package com.mvc.cryptovault.console.dao;
+
+import com.mvc.cryptovault.common.bean.AppUser;
+import com.mvc.cryptovault.console.common.MyMapper;
+
+public interface AppUserMapper extends MyMapper<AppUser> {
+}
---
+++ console/src/main/java/com/mvc/cryptovault/console/dao/AppUserOprLogMapper.java
@@ -0,0 +1,7 @@
+package com.mvc.cryptovault.console.dao;
+
+import com.mvc.cryptovault.common.bean.AppUserOprLog;
+import com.mvc.cryptovault.console.common.MyMapper;
+
+public interface AppUserOprLogMapper extends MyMapper<AppUserOprLog> {
+}
