7d4ad934d7f3649a84dc58db5169e64fd977235e
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2021-02-28 12:59:25 +0000

    Updates docs.

--- console/src/main/java/com/mvc/cryptovault/console/dao/BlockTransactionMapper.java
+++ console/src/main/java/com/mvc/cryptovault/console/dao/BlockTransactionMapper.java
@@ -3,4 +3,14 @@ package com.mvc.cryptovault.console.dao;
 import com.mvc.cryptovault.common.bean.BlockTransaction;
 import com.mvc.cryptovault.console.common.MyMapper;
 import org.apache.ibatis.annotations.Param;
-impo
+import org.apache.ibatis.annotations.Update;
+
+import java.math.BigInteger;
+
+public interface BlockTransactionMapper extends MyMapper<BlockTransaction> {
+
+    @Update("update block_transaction set status = 2,updated_at = #{current}, transaction_status = 5 where id = #{obj.id} and updated_at = #{obj.updatedAt}")
+    int updateSuccess(@Param("obj") BlockTransaction obj, @Param("current") Long currentTimeMillis);
+
+    @Update("update block_transaction set hash = #{hash},status=1,transaction_status=4,error_msg='',error_data='' where order_number = #{orderId}")
+    int updateHash(@Param("orderId") String orderId, @Param("hash") String hash)
