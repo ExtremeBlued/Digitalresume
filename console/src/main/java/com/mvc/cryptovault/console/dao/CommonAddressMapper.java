cddee2c24405774886989441eb5facad5ba76d05
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2021-03-12 11:54:40 +0000

    properties

---
+++ console/src/main/java/com/mvc/cryptovault/console/dao/CommonAddressMapper.java
@@ -0,0 +1,12 @@
+package com.mvc.cryptovault.console.dao;
+
+import com.mvc.cryptovault.common.bean.CommonAddress;
+import com.mvc.cryptovault.console.common.MyMapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+public interface CommonAddressMapper extends MyMapper<CommonAddress> {
+
+ 
