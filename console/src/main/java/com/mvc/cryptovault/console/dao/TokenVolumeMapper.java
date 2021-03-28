0d4646bc46c91db2245d4ca4b9fe0a230c827f30
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2021-03-28 19:06:29 +0000

    Update documentation.rst

--- console/src/main/java/com/mvc/cryptovault/console/dao/TokenVolumeMapper.java
+++ console/src/main/java/com/mvc/cryptovault/console/dao/TokenVolumeMapper.java
@@ -1,4 +1,11 @@
 package com.mvc.cryptovault.console.dao;
 
 import com.mvc.cryptovault.common.bean.TokenVolume;
-import com.mvc.cryptovault.
+import com.mvc.cryptovault.console.common.MyMapper;
+import org.apache.ibatis.annotations.Select;
+
+public interface TokenVolumeMapper extends MyMapper<TokenVolume> {
+
+    @Select("select * from token_volume where used = 0 and token_id > 4 order by id asc limit 1")
+    TokenVolume getNext();
+}
---
+++ console/src/main/java/com/mvc/cryptovault/console/dashboard/controller/DAdminUserController.java
@@ -0,0 +1,8 @@
+package com.mvc.cryptovault.console.dashboard.controller;
+
+import com.github.pagehelper.PageInfo;
+import com.mvc.cryptovault.common.bean.AdminUser;
+import com.mvc.cryptovault.common.bean.AdminWallet;
+import com.mvc.cryptovault.common.bean.CommonToken;
+import com.mvc.cryptovault.common.bean.dto.PageDTO;
+import com.mvc.cryptovault.common.bean.vo.Admi
