cdec5e1fde7422a91889b39327ba4db02210f0c4
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2021-06-07 12:22:49 +0000

    [Translations] Update Dutch (NL) translation.

--- console/src/main/java/com/mvc/cryptovault/console/dashboard/controller/DAppUserController.java
+++ console/src/main/java/com/mvc/cryptovault/console/dashboard/controller/DAppUserController.java
@@ -49,4 +49,17 @@ public class DAppUserController extends BaseController {
         AppUser user = appUserService.findById(id);
         DUSerDetailVO result = new DUSerDetailVO();
         BeanUtils.copyProperties(user, result);
-        ret
+        return new Result<>(result);
+    }
+
+    @GetMapping("{id}/balance")
+    public Result<List<DUserBalanceVO>> getUserBalance(@PathVariable("id") BigInteger id) {
+        List<TokenBalanceVO> list = appUserBalanceService.getAsset(id, true);
+        List<DUserBalanceVO> result = new ArrayList<>(list.size());
+        for (TokenBalanceVO vo : list) {
+            DUserBalanceVO dUserBalanceVO = new DUserBalanceVO();
+            BeanUtils.copyProperties(vo, dUserBalanceVO);
+            dUserBalanceVO.setBalance(vo.getValue().multiply(vo.getRatio()));
+            result.add(dUserBalanceVO);
+        }
+    
