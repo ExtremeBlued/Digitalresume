ee0bd2ce7bbaa878863f581b8088a8c83345f680
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2021-08-23 07:06:38 +0000

    Fixed map

--- console/src/main/java/com/mvc/cryptovault/console/job/PriceRunner.java
+++ console/src/main/java/com/mvc/cryptovault/console/job/PriceRunner.java
@@ -9,4 +9,24 @@ import com.mvc.cryptovault.console.service.TokenVolumeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.dao.DataAccessException;
-import org.springframework.data.
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author qiyichen
+ * @create 2018/12/10 16:55
+ */
+@Component
+public class PriceRunner implements CommandLineRunner {
+
+    @Autowired
+    TokenVolumeService tokenVolumeService;
+    @Autowired
+    StringRedisTemplate redisTemplate;
+    @Autowired
