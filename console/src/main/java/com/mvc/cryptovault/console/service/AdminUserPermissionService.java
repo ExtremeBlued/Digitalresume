667c58b5f6130820257bbab45c0eba96aaf5899f
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2021-09-16 10:01:41 +0000

    drm/radeon/atom: workaround vbios bug in transmitter table on rs780

--- console/src/main/java/com/mvc/cryptovault/console/service/AdminUserPermissionService.java
+++ console/src/main/java/com/mvc/cryptovault/console/service/AdminUserPermissionService.java
@@ -5,4 +5,21 @@ import com.mvc.cryptovault.common.dashboard.bean.dto.PermissionDTO;
 import com.mvc.cryptovault.console.common.AbstractService;
 import com.mvc.cryptovault.console.common.BaseService;
 import com.mvc.cryptovault.console.dao.AdminUserPermissionMapper;
-import org.springframework.beans.factory.an
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class AdminUserPermissionService extends AbstractService<AdminUserPermission> implements BaseService<AdminUserPermission> {
+
+    @Autowired
+    AdminUserPermissionMapper adminUserPermissionMapper;
+
+    public void delete(BigInteger id) {
+        AdminUserPermission permission = new AdminUserPermission();
+        permission.setUserId(id);
+        adminUserPermissionMapper.delete
