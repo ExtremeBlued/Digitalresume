73db8a1ceee80c73e89274e63559d55fcdfa5066
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2021-10-04 20:35:35 +0000

    namespaced unbind

--- console/src/main/java/com/mvc/cryptovault/console/service/AppKlineService.java
+++ console/src/main/java/com/mvc/cryptovault/console/service/AppKlineService.java
@@ -14,3 +14,139 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.NumberUtils;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.List;
+
+@Service
+public class AppKlineService extends AbstractService<AppKline> implements BaseService<AppKline> {
+
+    @Value("${kline.step}")
+    private Long step;
+    @Value("${kline.start}")
+    private Long start;
+    @Autowired
+    CommonPairService commonPairService;
+    @Autowired
+    CommonTokenHistoryMapper commonTokenHistoryMapper;
+    @Autowired
+    CommonTokenPriceService commonTokenPriceService;
+
+    private final String KLINE_HEADER = "KLINE_HEADER_";
+    private final Long KLINE_CACHE_TIME = 1000 * 60 * 5L;
+
+    /**
+     * @param tokenId
+     */
+    public void updateKline(BigInteger tokenId) {
+        //计算一共需要多少个采集点
+        Long point = start / step;
+        String key = "AppKline".toUpperCase() + "_" + tokenId;
+        Long length = redisTemplate.boundListOps(key).size();
+        if (null == length || !length.equals(point)) {
+            //需要重新绘制
+            resetTokenKline(tokenId);
+        } else {
+            //如果存在k线，直接取当前最近一次的价格
+            CommonTokenHistory appKline = commonTokenHistoryMapper.findByLast(tokenId);
+            redisTemplate.boundListOps(key).leftPop();
+            if (null == appKline) {
+                appKline = new CommonTokenHistory();
+                appKline.setCreatedAt(System.currentTimeMillis() - 100);
+                appKline.setPrice(BigDecimal.ZERO);
+            }
+            redisTemplate.boundListOps(key).rightPush(JSON.toJSONString(appKline));
+            redisTemplate.opsForHash().put(KLINE_HEADER + tokenId, "TIME", String.valueOf(System.currentTimeMillis() - 100));
+        }
+    }
+
+    private List<String> resetTokenKline(BigInteger tokenId) {
+        String key = "AppKline".toUpperCase() + "_" + tokenId;
+        Long now = System.currentTimeMillis();
+        Long startTime = now - start;
+        redisTemplate.delete(key);
+        for (Long i = startTime; i < now; i = i + step) {
+            CommonTokenHistory appKline = commonTokenHistoryMapper.findByTime(tokenId, i - 100);
+            if (null != appKline) {
+                appKline.setCreatedAt(i - 100);
+                redisTemplate.boundListOps(key).rightPush(JSON.toJSONString(appKline));
+            } else {
+                appKline = new CommonTokenHistory();
+                appKline.setCreatedAt(i - 100);
+                appKline.setPrice(BigDecimal.ZERO);
+                redisTemplate.boundListOps(key).rightPush(JSON.toJSONString(appKline));
+            }
+        }
+        Long size = redisTemplate.boundListOps(key).size();
+        if (size == 0) {
+            redisTemplate.boundListOps(key).rightPush("");
+        }
+        return redisTemplate.boundListOps(key).range(0, size);
+    }
+
+    private <T> T[] strArr2ObjArr(String str, Class<T> clazz) {
+        String strArr[] = str.split(",");
+        if (clazz.equals(Long.class)) {
+            Long arr[] = new Long[strArr.length];
+            for (int i = 0; i < strArr.length; i++) {
+                arr[i] = NumberUtils.parseNumber(strArr[i], Long.class);
+            }
+            return (T[]) arr;
+        } else {
+            BigDecimal arr[] = new BigDecimal[strArr.length];
+            for (int i = 0; i < strArr.length; i++) {
+                arr[i] = NumberUtils.parseNumber(strArr[i], BigDecimal.class);
+            }
+            return (T[]) arr;
+        }
+    }
+
+    public KLineVO getKLine(BigInteger pairId) {
+        CommonPair pair = commonPairService.findById(pairId);
+        String key = "AppKline".toUpperCase() + "_" + pair.getTokenId();
+        List<String> strList = redisTemplate.boundListOps(key).range(0, redisTemplate.boundListOps(key).size());
+        if (strList.size() == 0) {
+            resetTokenKline(pair.getTokenId());
+        }
+        String last = (String) redisTemplate.opsForHash().get(KLINE_HEADER + pair.getTokenId(), "TIME");
+        Long time = StringUtils.isBlank(last) ? System.currentTimeMillis() + KLINE_CACHE_TIME : Long.valueOf(last) + KLINE_CACHE_TIME;
+        String x = (String) redisTemplate.opsForHash().get(KLINE_HEADER + pair.getTokenId(), "X");
+        String y = (String) redisTemplate.opsForHash().get(KLINE_HEADER + pair.getTokenId(), "Y");
+        KLineVO vo = new KLineVO();
+//        if (time < System.currentTimeMillis() || StringUtils.isBlank(x) || "[]".equals(x)) {
+        if (true) {
+            //数据已过期,需要重新获取
+            Long[] timeX = new Long[strList.size()];
+            BigDecimal[] valueY = new BigDecimal[strList.size()];
+            for (int i = 0; i < strList.size(); i++) {
+                CommonTokenHistory history = JSON.parseObject(strList.get(i), CommonTokenHistory.class);
+                timeX[i] = history.getCreatedAt();
+                valueY[i] = history.getPrice();
+            }
+            vo.setTimeX(timeX);
+            vo.setValueY(valueY);
+            redisTemplate.opsForHash().put(KLINE_HEADER + pair.getTokenId(), "X", JSON.toJSONString(timeX));
+            redisTemplate.opsForHash().put(KLINE_HEADER + pair.getTokenId(), "Y", JSON.toJSONString(valueY));
+        } else {
+            List<Long> xList = JSON.parseArray(x, Long.class);
+            List<BigDecimal> yList = JSON.parseArray(y, BigDecimal.class);
+            vo.setTimeX(xList.toArray(new Long[]{}));
+            vo.setValueY(yList.toArray(new BigDecimal[]{}));
+        }
+        return vo;
+
+    }
+
+    public void saveHistory(BigInteger tokenId, BigDecimal usdtPrice) {
+        if (usdtPrice.compareTo(BigDecimal.ZERO) <= 0) {
+            return;
+        }
+        CommonTokenHistory history = new CommonTokenHistory();
+        history.setCreatedAt(System.currentTimeMillis());
+        history.setPrice(usdtPrice);
+        history.setTokenId(tokenId);
+        commonTokenHistoryMapper.insert(history);
+        commonTokenPriceService.updatePrice(tokenId, usdtPrice);
+    }
+}
---
+++ console/src/main/java/com/mvc/cryptovault/console/service/AppMessageService.java
@@ -0,0 +1,121 @@
+
+package com.mvc.cryptovault.console.service;
+
+import com.github.pagehelper.PageHelper;
+import com.mvc.cryptovault.common.bean.AppMessage;
+import com.mvc.cryptovault.common.bean.AppOrder;
+import com.mvc.cryptovault.common.bean.dto.PageDTO;
+import com.mvc.cryptovault.common.bean.dto.TimeSearchDTO;
+import com.mvc.cryptovault.common.util.ConditionUtil;
+import com.mvc.cryptovault.console.dao.AppMessageMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import tk.mybatis.mapper.entity.Condition;
+import tk.mybatis.mapper.entity.Example;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class AppMessageService {
+
+    private static final String MODEL_TRANSFER = "%s %s %s%s";
+    private static final String MODEL_PROJECT = "%s%s %s %s %s";
+    private static final String MODEL_PUBLISH = "%s %s %s金额已释放";
+    private static final String MODEL_TRADE = "%s 成功%s %s %s";
+
+    @Autowired
+    JPushService jPushService;
+    @Autowired
+    AppMessageMapper appMessageMapper;
+
+    public List<AppMessage> list(BigInteger userId, TimeSearchDTO timeSearchDTO, PageDTO pageDTO) {
+        Condition condition = new Condition(AppMessage.class);
+        Example.Criteria criteria = condition.createCriteria();
+        ConditionUtil.andCondition(criteria, "user_id = ", userId);
+        PageHelper.startPage(1, pageDTO.getPageSize());
+        PageHelper.orderBy("created_at desc");
+        if (timeSearchDTO.getType() == 0 && null != timeSearchDTO.getTimestamp()) {
+            ConditionUtil.andCondition(criteria, "created_at > ", timeSearchDTO.getTimestamp());
+        } else if (timeSearchDTO.getType() == 1 && null != timeSearchDTO.getTimestamp()) {
+            ConditionUtil.andCondition(criteria, "created_at < ", timeSearchDTO.getTimestamp());
+        }
+        return appMessageMapper.selectByCondition(condition);
+    }
+
+    @Async
+    public void transferMsg(BigInteger orderId, BigInteger userId, BigDecimal value, String tokenName, Integer transferType, Integer status) {
+        String transferTypeStr = transferType == 1 ? "收款" : transferType == 2 ? "提现" : "划账";
+        String statusStr = status == 2 ? "成功" : "失败";
+        String msg = String.format(MODEL_TRANSFER, value.stripTrailingZeros().toPlainString(), tokenName, transferTypeStr, statusStr);
+        Boolean result = jPushService.send(msg, orderId, String.valueOf(userId));
+        saveMsg(userId, result, orderId, msg);
+    }
+
+    @Async
+    public void sendProject(BigInteger userId, BigInteger projectId, BigInteger orderId, Integer status, Integer projectStatus, String tokenName, String projectName, BigDecimal value) {
+        Map<String, String> extra = new HashMap<>(1);
+        extra.put("projectId", projectId.toString());
+        String statusStr = status == 9 ? "失败" : "成功";
+        //项目状态0即将开始 1进行中 2已结束 3发币中 9取消
+        String projectStatusStr = projectStatus == 0 || projectStatus == 1 ? "预约" : "众筹";
+        String valueStr = status == 9 ? "" : ":" + value.stripTrailingZeros().toPlainString() + " " + tokenName;
+        String msg = String.format(MODEL_PROJECT, projectStatusStr, statusStr, tokenName, projectName, valueStr);
+        Boolean result = jPushService.send(msg, orderId, String.valueOf(userId));
+        saveMsg(userId, result, orderId, msg);
+    }
+
+    @Async
+    public void sendPublish(BigInteger projectId, String projectName, Long time, String tokenName, List<AppOrder> list) {
+        String dataStr = new SimpleDateFormat("yyyy月MM日").format(new Date(time));
+        String msg = String.format(MODEL_PUBLISH, tokenName, projectName, dataStr);
+        Boolean result = jPushService.sendTag(msg, projectId);
+        saveMsgByProjectId(projectId, result, msg, list);
+    }
+
+    private void saveMsgByProjectId(BigInteger projectId, Boolean result, String msg, List<AppOrder> list) {
+        list.forEach(obj -> {
+            saveMsg(obj.getUserId(), result, obj.getId(), msg);
+        });
+    }
+
+    @Async
+    public void sendTrade(BigInteger orderId, BigInteger userId, String pairName, Integer transferType, BigDecimal value, String tokenName) {
+        String transferTypeStr = transferType == 1 ? "购买" : "出售";
+        String msg = String.format(MODEL_TRADE, pairName, transferTypeStr, value.stripTrailingZeros().toPlainString(), tokenName);
+        Boolean result = jPushService.send(msg, orderId, String.valueOf(userId));
+        saveMsg(userId, result, orderId, msg);
+    }
+
+    private AppMessage saveMsg(BigInteger userId, Boolean result, BigInteger orderId, String msg) {
+        Long time = System.currentTimeMillis();
+        AppMessage appMessage = new AppMessage();
+        appMessage.setIsRead(0);
+        appMessage.setUserId(userId);
+        appMessage.setUpdatedAt(time);
+        appMessage.setStatus(result ? 1 : 0);
+        appMessage.setSendFlag(1);
+        appMessage.setPushTime(time);
+        appMessage.setMessageType(1);
+        appMessage.setCreatedAt(time);
+        appMessage.setContentType("ORDER");
+        appMessage.setContentId(orderId);
+        appMessage.setMessage(msg);
+        appMessageMapper.insert(appMessage);
+        return appMessage;
+    }
+
+    public Integer update(AppMessage appMessage) {
+        return appMessageMapper.updateByPrimaryKey(appMessage);
+    }
+
+    public List<AppMessage> findByCondition(Condition condition) {
+        return appMessageMapper.selectByCondition(condition);
+    }
+}
