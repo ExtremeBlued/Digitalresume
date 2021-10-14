e32ffc359c06e44632a9a52e9115870a9ebed6bd
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2021-10-14 18:14:58 +0000

    update unittests

--- console/src/main/java/com/mvc/cryptovault/console/service/AppProjectPartakeService.java
+++ console/src/main/java/com/mvc/cryptovault/console/service/AppProjectPartakeService.java
@@ -2,4 +2,16 @@ package com.mvc.cryptovault.console.service;
 
 import com.mvc.cryptovault.common.bean.AppOrder;
 import com.mvc.cryptovault.common.bean.AppProject;
-import com.mvc.crypto
+import com.mvc.cryptovault.common.bean.AppProjectPartake;
+import com.mvc.cryptovault.common.bean.dto.ImportPartake;
+import com.mvc.cryptovault.common.util.ConditionUtil;
+import com.mvc.cryptovault.console.common.AbstractService;
+import com.mvc.cryptovault.console.common.BaseService;
+import com.mvc.cryptovault.console.dao.AppProjectPartakeMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import tk.mybatis.mapper.entity.Condition;
+import tk.mybatis.mapper.entity.Example;
+
+import java.math.
