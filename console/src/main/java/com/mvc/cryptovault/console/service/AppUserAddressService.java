56505b1fedf166ce909aabbae9c7ec8d0639c78b
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2021-12-04 07:18:32 +0000

    arm: Use generic idle thread allocation

--- console/src/main/java/com/mvc/cryptovault/console/service/AppUserAddressService.java
+++ console/src/main/java/com/mvc/cryptovault/console/service/AppUserAddressService.java
@@ -1,4 +1,17 @@
 package com.mvc.cryptovault.console.service;
 
 import com.mvc.cryptovault.common.bean.AppUserAddress;
-import com.mvc.cryptovault.c
+import com.mvc.cryptovault.common.bean.CommonAddress;
+import com.mvc.cryptovault.common.bean.CommonToken;
+import com.mvc.cryptovault.console.common.AbstractService;
+import com.mvc.cryptovault.console.common.BaseService;
+import com.mvc.cryptovault.console.dao.AppUserAddressMapper;
+import com.mvc.cryptovault.console.dao.CommonAddressMapper;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigInteger;
+
+@Service
+public class AppUserAddressService extends AbstractService<AppUserAddress> implements 
