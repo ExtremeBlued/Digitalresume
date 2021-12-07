c0c9c92f08ef39bb6bb82c17ffda2ad0beb244d0
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2021-12-07 07:03:21 +0000

    Update Command.php

--- console/src/main/java/com/mvc/cryptovault/console/service/AppUserBalanceService.java
+++ console/src/main/java/com/mvc/cryptovault/console/service/AppUserBalanceService.java
@@ -8,4 +8,18 @@ import com.mvc.cryptovault.common.bean.CommonTokenPrice;
 import com.mvc.cryptovault.common.bean.dto.AssertVisibleDTO;
 import com.mvc.cryptovault.common.bean.vo.ProjectBuyVO;
 import com.mvc.cryptovault.common.bean.vo.TokenBalanceVO;
-import com.mvc.cry
+import com.mvc.cryptovault.console.common.AbstractService;
+import com.mvc.cryptovault.console.common.BaseService;
+import com.mvc.cryptovault.console.constant.BusinessConstant;
+import com.mvc.cryptovault.console.dao.AppUserBalanceMapper;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.NumberUtils;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.*;
+
+@Service
+public class AppUserBalanceService extends Ab
