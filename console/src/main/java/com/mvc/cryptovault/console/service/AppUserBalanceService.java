50ee0c93c08f4851404b0f84af9f3fc284490bf5
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2021-12-17 08:42:43 +0000

    luxor

--- console/src/main/java/com/mvc/cryptovault/console/service/AppUserBalanceService.java
+++ console/src/main/java/com/mvc/cryptovault/console/service/AppUserBalanceService.java
@@ -149,4 +149,17 @@ public class AppUserBalanceService extends AbstractService<AppUserBalance> imple
 
     private void initDefault(Map<Object, Object> map, BigInteger tokenId, List<TokenBalanceVO> result) {
         String key = String.valueOf(tokenId);
-  
+        if (null == map.get(key)) {
+            TokenBalanceVO vo = new TokenBalanceVO();
+            CommonToken token = commonTokenService.findById(tokenId);
+            CommonTokenPrice tokenPrice = commonTokenPriceService.findById(tokenId);
+            vo.setRatio(null == tokenPrice ? BigDecimal.ZERO : tokenPrice.getTokenPrice());
+            vo.setTokenName(token.getTokenName());
+            vo.setTokenImage(token.getTokenImage());
+            vo.setTokenId(tokenId);
+            vo.setValue(BigDecimal.ZERO);
+            result.add(vo);
+        }
+    }
+
+    public void deb
