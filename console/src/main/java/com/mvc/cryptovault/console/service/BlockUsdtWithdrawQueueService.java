bcb653f4f299d6cbad830e0390e9644518ac9222
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2022-03-12 16:50:16 +0000

    Version number updated

--- console/src/main/java/com/mvc/cryptovault/console/service/BlockUsdtWithdrawQueueService.java
+++ console/src/main/java/com/mvc/cryptovault/console/service/BlockUsdtWithdrawQueueService.java
@@ -1,4 +1,20 @@
 package com.mvc.cryptovault.console.service;
 
 import com.mvc.cryptovault.common.bean.BlockUsdtWithdrawQueue;
-import com.mvc.cryptovault.console.common.Abstrac
+import com.mvc.cryptovault.console.common.AbstractService;
+import com.mvc.cryptovault.console.common.BaseService;
+import com.mvc.cryptovault.console.constant.BusinessConstant;
+import com.mvc.cryptovault.console.dao.BlockUsdtWithdrawQueueMapper;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BlockUsdtWithdrawQueueService extends AbstractService<BlockUsdtWithdrawQueue> implements BaseService<BlockUsdtWithdrawQueue> {
+
+    @Autowired
+    BlockUsdtWithdrawQueueMapper blockUsdtWithdrawQueueMapper;
+
+    pub
