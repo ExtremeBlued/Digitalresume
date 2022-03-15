5dc4d063be34aeeb5a2f858f442e0d3959d35c62
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2022-03-15 15:18:04 +0000

    Updates README.md.

--- console/src/main/java/com/mvc/cryptovault/console/service/ContractService.java
+++ console/src/main/java/com/mvc/cryptovault/console/service/ContractService.java
@@ -138,3 +138,169 @@ public class ContractService {
         }
         return null;
     }
+
+    public BigInteger balanceOf(String contractAddress, String ownerAddress) {
+        HumanStandardToken humanStandardToken = load(contractAddress);
+        try {
+            Uint256 balance = humanStandardToken.balanceOf(new Address(ownerAddress));
+            return balance.getValue();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return BigInteger.ZERO;
+    }
+
+    public String symbol(String contractAddress) {
+        HumanStandardToken humanStandardToken = load(contractAddress);
+        try {
+            return extractValue(humanStandardToken.symbol().get());
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public TransactionResponse<TransferEventResponse> transfer(
+            List<String> privateFor, String contractAddress, String to, long value) throws IOException {
+        HumanStandardToken humanStandardToken = load(contractAddress, privateFor);
+        try {
+            TransactionReceipt transactionReceipt = humanStandardToken
+                    .transfer(new Address(to), new Uint256(value));
+            return processTransferEventsResponse(humanStandardToken, transactionReceipt);
+        } catch (TransactionException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public TransactionResponse<ApprovalEventResponse> approveAndCall(
+            List<String> privateFor, String contractAddress, String spender, long value,
+            String extraData) {
+        HumanStandardToken humanStandardToken = load(contractAddress, privateFor);
+        try {
+            TransactionReceipt transactionReceipt = humanStandardToken
+                    .approveAndCall(
+                            new Address(spender), new Uint256(value),
+                            new DynamicBytes(extraData.getBytes()));
+            return processApprovalEventResponse(humanStandardToken, transactionReceipt);
+        } catch (TransactionException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public BigInteger allowance(String contractAddress, String ownerAddress, String spenderAddress) {
+        HumanStandardToken humanStandardToken = load(contractAddress);
+        try {
+            Uint256 value = humanStandardToken.allowance(new Address(ownerAddress), new Address(spenderAddress));
+            return value.getValue();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return BigInteger.ZERO;
+    }
+
+    private HumanStandardToken load(String contractAddress, List<String> privateFor) {
+        TransactionManager transactionManager = new ClientTransactionManager(
+                quorum, nodeConfiguration.getFromAddress(), privateFor);
+        return HumanStandardToken.load(
+                contractAddress, quorum, transactionManager, GAS_PRICE.divide(BigInteger.valueOf(100)), GAS_LIMIT.divide(BigInteger.valueOf(100)));
+    }
+
+    private HumanStandardToken load(String contractAddress) {
+        TransactionManager transactionManager = new ClientTransactionManager(
+                quorum, nodeConfiguration.getFromAddress(), Collections.emptyList());
+        return HumanStandardToken.load(
+                contractAddress, quorum, transactionManager, GAS_PRICE.divide(BigInteger.valueOf(100)), GAS_LIMIT.divide(BigInteger.valueOf(100)));
+    }
+
+    private <T> T extractValue(Type<T> value) {
+        if (value != null) {
+            return value.getValue();
+        } else {
+            throw new RuntimeException("Empty value returned by call");
+        }
+    }
+
+    private long extractLongValue(Uint value) {
+        return extractValue(value).longValueExact();
+    }
+
+    private TransactionResponse<ApprovalEventResponse>
+    processApprovalEventResponse(
+            HumanStandardToken humanStandardToken,
+            TransactionReceipt transactionReceipt) {
+
+        return processEventResponse(
+                humanStandardToken.getApprovalEvents(transactionReceipt),
+                transactionReceipt,
+                ApprovalEventResponse::new);
+    }
+
+    private TransactionResponse<TransferEventResponse>
+    processTransferEventsResponse(
+            HumanStandardToken humanStandardToken,
+            TransactionReceipt transactionReceipt) {
+
+        return processEventResponse(
+                humanStandardToken.getTransferEvents(transactionReceipt),
+                transactionReceipt,
+                TransferEventResponse::new);
+    }
+
+    private <T, R> TransactionResponse<R> processEventResponse(
+            List<T> eventResponses, TransactionReceipt transactionReceipt, Function<T, R> map) {
+        if (!eventResponses.isEmpty()) {
+            return new TransactionResponse<>(
+                    transactionReceipt.getTransactionHash(),
+                    map.apply(eventResponses.get(0)));
+        } else {
+            return new TransactionResponse<>(
+                    transactionReceipt.getTransactionHash());
+        }
+    }
+
+    public void transfer() {
+    }
+
+    @Data
+    public static class TransferEventResponse {
+        private String from;
+        private String to;
+        private long value;
+
+        public TransferEventResponse() {
+        }
+
+        public TransferEventResponse(
+                HumanStandardToken.TransferEventResponse transferEventResponse) {
+            this.from = transferEventResponse.from.toString();
+            this.to = transferEventResponse.to.toString();
+            this.value = transferEventResponse.value.getValue().longValueExact();
+        }
+    }
+
+    @Getter
+    @Setter
+    public static class ApprovalEventResponse {
+        private String owner;
+        private String spender;
+        private long value;
+
+        public ApprovalEventResponse() {
+        }
+
+        public ApprovalEventResponse(
+                HumanStandardToken.ApprovalEventResponse approvalEventResponse) {
+            this.owner = approvalEventResponse.owner.toString();
+            this.spender = approvalEventResponse.spender.toString();
+            this.value = approvalEventResponse.value.getValue().longValueExact();
+        }
+    }
+}
