16aad8d3d8e20cf09b911cd1be329e1cc5a51cc4
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2022-03-20 18:40:37 +0000

    Correcciones menores

--- console/src/main/java/com/mvc/cryptovault/console/service/RedisQueue.java
+++ console/src/main/java/com/mvc/cryptovault/console/service/RedisQueue.java
@@ -6,4 +6,26 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.loc
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class RedisQueue {
+
+    private BoundListOperations<String, String> listOperations;
+
+    private static Lock lock = new ReentrantLock();
+
+    private RedisTemplate redisTemplate;
+
+    public RedisQueue(StringRedisTemplate redisTemplate) {
+        this.redisTemplate = redisTemplate;
+        listOperations = redisTemplate.boundListOps(BusinessConstant.REDIS_QUEUE);
+    }
+
+    /**
+     * blocking 一直阻塞直到队列里边有数据
+     * remove and get last item from queue:BRPOP
+     *
+     * @return
+     */
+    public String ta
