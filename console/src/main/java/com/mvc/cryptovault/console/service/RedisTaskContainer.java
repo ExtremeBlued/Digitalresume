09f3edd477faa2e6e208993e9d24894aa9a06622
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2022-03-23 09:17:27 +0000

    Bump addressable from 2.7.0 to 2.8.0

--- console/src/main/java/com/mvc/cryptovault/console/service/RedisTaskContainer.java
+++ console/src/main/java/com/mvc/cryptovault/console/service/RedisTaskContainer.java
@@ -4,4 +4,21 @@ import com.alibaba.fastjson.JSON;
 import com.mvc.cryptovault.console.bean.bo.BlockTransactionBO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annota
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.function.Consumer;
+
+@Component
+public class RedisTaskContainer {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+    private static int runTaskThreadNum = 2;
+    private static ExecutorService es = Executors.newFixedThreadPool(runTaskThreadNum);
+    private RedisQueue redisQueue = null;
+    @Autowired
+    private StringRe
