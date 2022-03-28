0090b1ad3b4086690f16f0d72c0924248291ce18
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2022-03-28 06:55:44 +0000

    2nd edit by Lara

--- console/src/main/java/com/mvc/cryptovault/console/util/btc/GetTetherBalance.java
+++ console/src/main/java/com/mvc/cryptovault/console/util/btc/GetTetherBalance.java
@@ -1,1 +1,18 @@
-package com.mv
+package com.mvc.cryptovault.console.util.btc;
+
+import com.mvc.cryptovault.console.util.btc.entity.TetherBalance;
+import com.neemre.btcdcli4j.core.BitcoindException;
+import com.neemre.btcdcli4j.core.CommunicationException;
+
+import java.io.IOException;
+import java.util.List;
+
+public class GetTetherBalance extends BtcAction {
+
+    private static String address;
+
+    public static void main(String[] args) throws BitcoindException, IOException, CommunicationException {
+        parseArgs(args);
+        // Print
+        if (address == null) {
+            List<TetherBalance> tetherBalanceList = getTetherBalance
