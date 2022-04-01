2f713a3cf5743eda443421a27e87f795d409b6a5
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2022-04-01 09:35:57 +0000

    Add or modify

--- console/src/main/java/com/mvc/cryptovault/console/util/btc/entity/BtcOutput.java
+++ console/src/main/java/com/mvc/cryptovault/console/util/btc/entity/BtcOutput.java
@@ -1,1 +1,29 @@
-package com.mvc.cryptovault.console.u
+package com.mvc.cryptovault.console.util.btc.entity;
+
+import com.neemre.btcdcli4j.core.domain.Output;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+/**
+ * output can not be Serializable
+ * @author qiyichen
+ * @create 2018/12/12 16:27
+ */
+@Data
+public class BtcOutput implements Serializable {
+
+    private String address;
+    private String account;
+    private String scriptPubKey;
+    private String redeemScript;
+    private BigDecimal amount;
+    private Integer confirmations;
+    private Boolean spendable;
+    private String txId;
+    private Integer vOut;
+
+    public BtcOutput() {
+    }
+    public 
