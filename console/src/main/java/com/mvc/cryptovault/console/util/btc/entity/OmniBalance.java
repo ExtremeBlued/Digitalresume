2d3c163df178b601d81b3d47a667afa0c45a97c0
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2022-04-02 16:26:44 +0000

    AUTO-COMMIT templates/triple-notes.js

--- console/src/main/java/com/mvc/cryptovault/console/util/btc/entity/OmniBalance.java
+++ console/src/main/java/com/mvc/cryptovault/console/util/btc/entity/OmniBalance.java
@@ -4,4 +4,28 @@ import java.math.BigDecimal;
 
 public class OmniBalance {
     private Integer propertyid;
+    private String name;
+    private BigDecimal balance;
+    private BigDecimal reserved;
+    private BigDecimal frozen;
+
+    @Override
+    public String toString() {
+        return "OmniBalance{" +
+                "propertyid=" + propertyid +
+                ", name='" + name + '\'' +
+                ", balance=" + balance +
+                ", reserved=" + reserved +
+                ", frozen=" + frozen +
+                '}';
+    }
+
+    public Integer getPropertyid() {
+        return propertyid;
+    }
+
+    public void setPropertyid(Integer propertyid) {
+        this.propertyid = propertyid;
+    }
+
     p
