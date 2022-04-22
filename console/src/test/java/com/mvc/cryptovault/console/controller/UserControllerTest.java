44e4f519ba9d65ccb94cc868177fd7718d230dba
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2022-04-22 10:54:18 +0000

    Version 0.8.2

--- console/src/test/java/com/mvc/cryptovault/console/controller/UserControllerTest.java
+++ console/src/test/java/com/mvc/cryptovault/console/controller/UserControllerTest.java
@@ -3,9 +3,13 @@ package com.mvc.cryptovault.console.controller;
 import com.mvc.cryptovault.console.base.BaseTest;
 import org.junit.Test;
 
-public class CommonTokenPriceControllerTest extends BaseTest {
+public class UserControllerTest extends BaseTest {
 
     @Test
-    public void getAll() {
+    public void getByUsername() {
+    }
+
+    @Test
+    public void getUserById() {
     }
 }
