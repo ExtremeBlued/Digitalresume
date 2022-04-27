8d81eb6a56badb08011dd64c89a49d76bb055baa
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2022-04-27 06:44:59 +0000

    code simplification

--- dashbord/src/main/java/com/mvc/cryptovault/dashboard/config/GlobalExceptionHandler.java
+++ dashbord/src/main/java/com/mvc/cryptovault/dashboard/config/GlobalExceptionHandler.java
@@ -3,4 +3,22 @@ package com.mvc.cryptovault.dashboard.config;
 import com.mvc.cryptovault.common.bean.vo.Result;
 import com.mvc.cryptovault.common.util.TokenErrorException;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.security.auth.login.LoginException;
+
+/**
+ * @author qyc
+ */
+@ControllerAdvice
+@ResponseBody
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(LoginException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public Result loginExceptionException() {
+        return new Result(HttpStatus.UNAUTHORIZED.valu
