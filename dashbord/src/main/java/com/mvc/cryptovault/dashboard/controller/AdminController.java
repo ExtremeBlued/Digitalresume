46313326f1dfe432523bb16cdae6d0ffe54259f3
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2022-05-21 12:39:19 +0000

    fix compile issue

--- dashbord/src/main/java/com/mvc/cryptovault/dashboard/controller/AdminController.java
+++ dashbord/src/main/java/com/mvc/cryptovault/dashboard/controller/AdminController.java
@@ -6,4 +6,17 @@ import com.mvc.cryptovault.common.bean.vo.Result;
 import com.mvc.cryptovault.common.bean.vo.TokenVO;
 import com.mvc.cryptovault.common.dashboard.bean.dto.AdminDTO;
 import com.mvc.cryptovault.common.dashboard.bean.dto.AdminPasswordDTO;
-import com.mvc.cryptovault.common.da
+import com.mvc.cryptovault.common.dashboard.bean.dto.DUserDTO;
+import com.mvc.cryptovault.common.dashboard.bean.vo.AdminDetailVO;
+import com.mvc.cryptovault.common.dashboard.bean.vo.AdminVO;
+import com.mvc.cryptovault.common.permission.NotLogin;
+import com.mvc.cryptovault.dashboard.service.OssService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import java.io.UnsupportedEncodingException;
+import java.math.
