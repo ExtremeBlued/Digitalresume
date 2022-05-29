a70d332f9b4231b3337d9f1488a0915187b95c5b
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2022-05-29 22:10:10 +0000

    misc tuning;

--- dashbord/src/main/java/com/mvc/cryptovault/dashboard/controller/BaseController.java
+++ dashbord/src/main/java/com/mvc/cryptovault/dashboard/controller/BaseController.java
@@ -5,4 +5,29 @@ import com.mvc.cryptovault.common.util.BaseContextHandler;
 import com.mvc.cryptovault.dashboard.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import or
+import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
+import org.springframework.web.client.RestTemplate;
+
+import java.math.BigInteger;
+
+/**
+ * @author qiyichen
+ * @create 2018/11/6 17:11
+ */
+public class BaseController {
+
+    @Autowired
+    protected StringRedisTemplate redisTemplate;
+    protected RestTemplate restTemplate = new RestTemplate();
+
+    @Autowired
+    AdminService adminService;
+    @Autowired
+    BlockService blockService;
+    @Autowired
+    ProjectService projectService;
+    @Autowired
+    TokenService tokenService;
+    @Autowired
+    TransactionService tra
