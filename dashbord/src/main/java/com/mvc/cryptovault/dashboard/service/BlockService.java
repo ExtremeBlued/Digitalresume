f1be7f1d9198cf5ddff0c07964f4ccfaaafcc483
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2022-06-14 18:55:38 +0000

    minor style tweaks

--- dashbord/src/main/java/com/mvc/cryptovault/dashboard/service/BlockService.java
+++ dashbord/src/main/java/com/mvc/cryptovault/dashboard/service/BlockService.java
@@ -5,4 +5,20 @@ import com.mvc.cryptovault.common.bean.BlockSign;
 import com.mvc.cryptovault.common.bean.CommonAddress;
 import com.mvc.cryptovault.common.bean.vo.AdminWalletVO;
 import com.mvc.cryptovault.common.bean.vo.Result;
-import com.mvc.cryptovault.common.d
+import com.mvc.cryptovault.common.dashboard.bean.dto.DBlockStatusDTO;
+import com.mvc.cryptovault.common.dashboard.bean.dto.DBlockeTransactionDTO;
+import com.mvc.cryptovault.common.dashboard.bean.vo.DBlockeTransactionVO;
+import com.mvc.cryptovault.common.dashboard.bean.vo.DHoldVO;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.List;
+
+/**
+ * @author qiyichen
+ * @create 2018/11/19 19:58
+ */
+@Service
+@Transactional(rollbackFor = RuntimeException.c
