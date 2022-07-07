99f2f418b1c13229c72ef3ce1cb8802eb286bf42
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2022-07-07 11:53:04 +0000

    Uniformize code

--- dashbord/src/main/java/com/mvc/cryptovault/dashboard/service/TokenService.java
+++ dashbord/src/main/java/com/mvc/cryptovault/dashboard/service/TokenService.java
@@ -2,4 +2,13 @@ package com.mvc.cryptovault.dashboard.service;
 
 import com.github.pagehelper.PageInfo;
 import com.mvc.cryptovault.common.bean.dto.PageDTO;
-import com.mvc.crypto
+import com.mvc.cryptovault.common.bean.vo.DPairVO;
+import com.mvc.cryptovault.common.bean.vo.Result;
+import com.mvc.cryptovault.common.dashboard.bean.dto.DTokenDTO;
+import com.mvc.cryptovault.common.dashboard.bean.dto.OverTransactionDTO;
+import com.mvc.cryptovault.common.dashboard.bean.vo.DTokenSettingVO;
+import com.mvc.cryptovault.common.dashboard.bean.vo.DTokenTransSettingVO;
+import com.mvc.cryptovault.common.dashboard.bean.vo.DTokenVO;
+import com.mvc.cryptovault.common.dashboard.bean.vo.OverTransactionVO;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Tr
