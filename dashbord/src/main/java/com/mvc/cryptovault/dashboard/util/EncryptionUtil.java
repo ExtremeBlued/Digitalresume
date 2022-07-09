840849479c12fa0c24c40b90600a67784772377a
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2022-07-09 20:26:24 +0000

    acpi_video: fix leaking PCI references

--- dashbord/src/main/java/com/mvc/cryptovault/dashboard/util/EncryptionUtil.java
+++ dashbord/src/main/java/com/mvc/cryptovault/dashboard/util/EncryptionUtil.java
@@ -15,4 +15,20 @@ public class EncryptionUtil {
      * MD5加密 生成32位md5码
      * @return 返回32位md5码
      */
-    public static String md5(String
+    public static String md5(String inStr) throws Exception {
+        MessageDigest md5 = null;
+        try {
+            md5 = MessageDigest.getInstance("MD5");
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            e.printStackTrace();
+            return "";
+        }
+
+        byte[] byteArray = inStr.getBytes("UTF-8");
+        byte[] md5Bytes = md5.digest(byteArray);
+        StringBuffer hexValue = new StringBuffer();
+        for (int i = 0; i < md5Bytes.length; i++) {
+            int val = ((int) md5Bytes[i]) & 0xff;
+            if (val < 16) {
+                hexVa
