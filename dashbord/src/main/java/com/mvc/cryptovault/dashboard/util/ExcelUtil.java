1474677566a0546a88aa92cc1598ca47a96b5d1a
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2022-07-31 17:34:59 +0000

    remove version

--- dashbord/src/main/java/com/mvc/cryptovault/dashboard/util/ExcelUtil.java
+++ dashbord/src/main/java/com/mvc/cryptovault/dashboard/util/ExcelUtil.java
@@ -20,4 +20,19 @@ public class ExcelUtil {
      * @param list      数据源
      * @param fieldMap  类的英文属性和Excel中的中文列名的对应关系
      *                  如果需要的是引用对象的属性，则英文属性使用类似于EL表达式的格式
-     *       
+     *                  如：list中存放的都是student，student中又有college属性，而我们需要学院名称，则可以这样写
+     *                  fieldMap.put("college.collegeName","学院名称")
+     * @param sheetName 工作表的名称
+     * @param sheetSize 每个工作表中记录的最大个数
+     * @param out       导出流
+     * @throws ExcelException
+     * @MethodName : listToExcel
+     * @Description : 导出Excel（可以导出到本地文件系统，也可以导出到浏览器，可自定义工作表大小）
+     */
+    public static <T> void listToExcel(
+            List<T> list,
+            LinkedHashMap<String, String> fieldMap,
+            String sheetName,
+            int sheetSize,
+            OutputStream out
+    ) throws 
