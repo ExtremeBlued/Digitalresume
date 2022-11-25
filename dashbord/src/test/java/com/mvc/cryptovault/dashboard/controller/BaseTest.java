ceabcabd52794dd176e76df2e42bd21f8c55eb28
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2022-11-25 20:02:44 +0000

    HID: usbhid: fix build problem

--- dashbord/src/test/java/com/mvc/cryptovault/dashboard/controller/BaseTest.java
+++ dashbord/src/test/java/com/mvc/cryptovault/dashboard/controller/BaseTest.java
@@ -2,4 +2,16 @@ package com.mvc.cryptovault.dashboard.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
-import com.mvc.cryptovault.common
+import com.mvc.cryptovault.common.bean.dto.UserDTO;
+import com.mvc.cryptovault.common.bean.vo.Result;
+import com.mvc.cryptovault.common.bean.vo.TokenVO;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.ser
