7978bdcec6b23c624a563e7ee21d2a4f382b387e
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2022-12-11 14:36:07 +0000

    color picker

--- dashbord/src/test/java/com/mvc/cryptovault/dashboard/controller/ProjectControllerTest.java
+++ dashbord/src/test/java/com/mvc/cryptovault/dashboard/controller/ProjectControllerTest.java
@@ -4,4 +4,19 @@ import com.alibaba.fastjson.JSON;
 import com.mvc.cryptovault.common.dashboard.bean.dto.DProjectDTO;
 import com.mvc.cryptovault.common.dashboard.bean.vo.DProjectDetailVO;
 import com.mvc.cryptovault.common.dashboard.bean.vo.DProjectOrderVO;
-imp
+import com.mvc.cryptovault.common.dashboard.bean.vo.DProjectVO;
+import org.junit.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+public class ProjectControllerTest extends BaseTest {
+
+    String contr
