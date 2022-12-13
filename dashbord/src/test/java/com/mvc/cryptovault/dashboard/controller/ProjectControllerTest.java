788c6c9d77835014ed548a1dcf3961d1869393f4
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2022-12-13 19:12:09 +0000

    Update music.md

--- dashbord/src/test/java/com/mvc/cryptovault/dashboard/controller/ProjectControllerTest.java
+++ dashbord/src/test/java/com/mvc/cryptovault/dashboard/controller/ProjectControllerTest.java
@@ -104,4 +104,19 @@ public class ProjectControllerTest extends BaseTest {
                 .andDo(print())
                 .andExpect(MockMvcResultMatchers.status().isOk());
         action.andExpect(MockMvcResultMatchers.jsonPath("data").isNotEmpty());
-        MvcResult result 
+        MvcResult result = action.andReturn();
+    }
+
+    @Test
+    public void deleteProject() throws Exception {
+        String url = host + controller + "/0";
+        ResultActions action = mockMvc.perform(MockMvcRequestBuilders.delete(url)
+                .header("Authorization", getToken().getToken())
+                .contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isOk());
+        action.andExpect(MockMvcResultMatchers.jsonPath("data").isNotEmpty());
+        MvcResult result = action.andReturn();
+    }
+
+    @T
