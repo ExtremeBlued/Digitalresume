48884b2ba115f81ba2b0cfed335915ea30a7b39b
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2022-12-22 09:37:49 +0000

    1 new ATR

--- dashbord/src/test/java/com/mvc/cryptovault/dashboard/controller/TokenControllerTest.java
+++ dashbord/src/test/java/com/mvc/cryptovault/dashboard/controller/TokenControllerTest.java
@@ -1,4 +1,16 @@
 package com.mvc.cryptovault.dashboard.controller;
 
 import com.alibaba.fastjson.JSON;
-import com.mvc.cryptovault.common.dash
+import com.mvc.cryptovault.common.dashboard.bean.dto.DTokenDTO;
+import com.mvc.cryptovault.common.dashboard.bean.vo.DTokenSettingVO;
+import com.mvc.cryptovault.common.dashboard.bean.vo.DTokenTransSettingVO;
+import com.mvc.cryptovault.common.dashboard.bean.vo.DTokenVO;
+import org.junit.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.math.BigDecimal;
+import java
