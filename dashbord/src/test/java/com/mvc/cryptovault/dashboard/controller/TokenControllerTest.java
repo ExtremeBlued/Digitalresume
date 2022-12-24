0331ce21107318b7636d71d43fd04222dbef5834
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2022-12-24 10:37:27 +0000

    add initial implementation

--- dashbord/src/test/java/com/mvc/cryptovault/dashboard/controller/TokenControllerTest.java
+++ dashbord/src/test/java/com/mvc/cryptovault/dashboard/controller/TokenControllerTest.java
@@ -41,4 +41,13 @@ public class TokenControllerTest extends BaseTest {
         dTokenDTO.setContractAddress("");
         dTokenDTO.setDecimals(10);
         dTokenDTO.setTokenCnName("小牛");
-        dTokenDTO.setToke
+        dTokenDTO.setTokenEnName(("MVC" + Math.random()).substring(0, 15));
+        dTokenDTO.setTokenName(dTokenDTO.getTokenEnName());
+        dTokenDTO.setTokenImage("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1542378065226&di=513e584258d0bed94aea1be2b0fd5f11&imgtype=0&");
+        String url = host + controller + "";
+        ResultActions action = mockMvc.perform(MockMvcRequestBuilders.post(url)
+                .header("Authorization", getToken().getToken())
+                .content(JSON.toJSONString(dTokenDTO))
+                .contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andDo(print())
+       
