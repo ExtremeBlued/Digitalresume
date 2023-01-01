02cba218636b58588558c6adc2801b7bba5e171e
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2023-01-01 13:44:41 +0000

    Added missing ;

--- dashbord/src/test/java/com/mvc/cryptovault/dashboard/controller/TransactionControllerTest.java
+++ dashbord/src/test/java/com/mvc/cryptovault/dashboard/controller/TransactionControllerTest.java
@@ -9,4 +9,20 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.springframework.test.web.ser
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+public class TransactionControllerTest extends BaseTest {
+
+    String controller = "/transaction";
+
+    @Test
+    public void findTransaction() throws Exception {
+        String url = host + controller + "";
+        ResultActions action = mockMvc.perform(MockMvcRequestBuilders.get(url)
+                .header("Authorization", getToken().getToken())
+                .param("pairId", "")
+                .param("transactionType", "")
+                .param("orderNumber", "")
+                .param("cellphone", "")
+                .param("status", "")
+                
