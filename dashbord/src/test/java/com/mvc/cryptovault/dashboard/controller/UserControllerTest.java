ef25dfd3c5bb8508063ac4a2f7632a953618e164
ExtremeBlued <106496554+ExtremeBlued@users.noreply.github.com>
2023-01-12 20:52:26 +0000

    Small refactoring

--- dashbord/src/test/java/com/mvc/cryptovault/dashboard/controller/UserControllerTest.java
+++ dashbord/src/test/java/com/mvc/cryptovault/dashboard/controller/UserControllerTest.java
@@ -7,3 +7,17 @@ import com.mvc.cryptovault.common.dashboard.bean.vo.DUserLogVO;
 import org.junit.Test;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+public class UserControllerTest extends BaseTest {
+    String controller = "/user";
+
+    @Test
+    public void findUser() throws Exception {
+        String url = host + controller + "";
+        ResultActions action = mockMvc.perform(MockMvcRequestBuilders.get(url)
+            
